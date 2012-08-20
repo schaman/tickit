b929e22f3edb6e399e582526e442111bd79fe65f
James Halsall <james.t.halsall@googlemail.com>
2012-08-20 13:40:56 +0100

    Adding setters to DefaultGroupPermissionValue entity

--- src/Tickit/PermissionBundle/Entity/DefaultGroupPermissionValue.php
+++ src/Tickit/PermissionBundle/Entity/DefaultGroupPermissionValue.php
@@ -3,6 +3,7 @@
 namespace Tickit\PermissionBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Tickit\UserBundle\Entity\Group;
 
 /**
  * Represents the default value of a permission against a specific group. This is used when new users are
@@ -34,4 +35,33 @@ class DefaultGroupPermissionValue
      */
     protected $value;
 
+    /**
+     * Sets the group on this object
+     *
+     * @param Group $group
+     */
+    public function setGroup(Group $group)
+    {
+        $this->group = $group;
+    }
+
+    /**
+     * Sets the permission on this object
+     *
+     * @param Permission $permission
+     */
+    public function setPermission(Permission $permission)
+    {
+        $this->permission = $permission;
+    }
+
+    /**
+     * Sets the boolean value for this permission value pair
+     *
+     * @param bool $value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
 }
