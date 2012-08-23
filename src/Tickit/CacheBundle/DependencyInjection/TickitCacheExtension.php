ffef4e26eabfc4a6290db7e0b71be3cfcce0fab6
James Halsall <james.t.halsall@googlemail.com>
2012-08-23 15:01:18 +0100

    Adding skeleton CacheBundle

---
+++ src/Tickit/CacheBundle/DependencyInjection/TickitCacheExtension.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tickit\CacheBundle\DependencyInjection;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class TickitCacheBundle extends Extension
+{
+
+    /**
+     * Loads dependency injection configuration
+     *
+     * @param array $configs
+     * @param
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        //TODO: load config
+    }
+}
---
+++ src/Tickit/CacheBundle/TickitCacheBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tickit\CacheBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle build file for TickitCacheBundle
+ *
+ * @author James Halsall <james.t.halsall@googlemail.com>
+ */
+class TickitCacheBundle
+{
+
+}
