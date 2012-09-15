df570f1fef22d88f1d46e92c5f5ffad2cbab7ce7
James Halsall <james.t.halsall@googlemail.com>
2012-09-15 23:24:34 +0100

    Updating tests for FileEngine, refactoring

--- src/Tickit/CacheBundle/Tests/Engine/FileEngineTest.php
+++ src/Tickit/CacheBundle/Tests/Engine/FileEngineTest.php
@@ -13,51 +13,77 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
  */
 class FileEngineTest extends WebTestCase
 {
+    private $container;
+
     /**
      * Makes sure that the file engine is working together with the options resolver
-     * to correctly configure valid options for the file cache engine
+     * to correctly configure valid and invalid values the cache directory option
      */
-    public function testValidCacheDirectoryOption()
+    public function testCacheDirectoryOption()
     {
-        $client = static::createClient();
-
-        $options = array(
+        $validOptions = array(
             'cache_dir' => '/tmp'
         );
 
-        $cache = new FileEngine($client->getContainer(), $options);
-        $this->assertEquals('/tmp', $cache->getOptions()->getCacheDir());
-    }
+        $engine = $this->_getEngineInstance($validOptions);
+        $this->assertEquals('/tmp', $engine->getOptions()->getCacheDir());
 
-    /**
-     * Makes sure that an illegal file cache (unwritable) option is correctly
-     * rejected and the default is used instead
-     */
-    public function testIllegalCacheDirectoryOption()
-    {
-        $client = static::createClient();
-
-        $options = array(
+        $invalidOptions = array(
             'cache_dir' => '/etc'
         );
 
-        $cache = new FileEngine($client->getContainer(), $options);
-        $this->assertEquals('/tmp', $cache->getOptions()->getCacheDir());
+        $engine = $this->_getEngineInstance($invalidOptions);
+        $this->assertEquals('/tmp', $engine->getOptions()->getCacheDir());
     }
 
     /**
      * Makes sure that the auto_serialize option is properly configured
      */
-    public function testValidAutoSerializeOption()
+    public function testAutoSerializeOption()
     {
-        $client = static::createClient();
-
-        $options = array(
+        $validOptions = array(
             'auto_serialize' => false
         );
 
-        $cache = new FileEngine($client->getContainer(), $options);
-        $this->assertEquals(false, $cache->getOptions()->getAutoSerialize());
+        $engine = $this->_getEngineInstance($validOptions);
+        $this->assertEquals(false, $engine->getOptions()->getAutoSerialize());
+
+        $invalidOptions = array(
+            'auto_serialize' => 'notBoolean'
+        );
+
+        $engine = $this->_getEngineInstance($invalidOptions);
+        $defaultAutoSerialize = $this->_getContainer()->getParameter('tickit_cache.file.auto_serialize');
+        $this->assertEquals($defaultAutoSerialize, $engine->getOptions()->getAutoSerialize());
+    }
+
+    /**
+     * Instantiates and returns a new FileEngine
+     *
+     * @param array $options An array of options to inject into the engine
+     *
+     * @return \Tickit\CacheBundle\Engine\FileEngine
+     */
+    private function _getEngineInstance(array $options)
+    {
+        $client = static::createClient();
+
+        return new FileEngine($client->getContainer(), $options);
+    }
+
+    /**
+     * Instantiates and returns the service container
+     *
+     * @return \Symfony\Component\DependencyInjection\ContainerInterface
+     */
+    private function _getContainer()
+    {
+        if (null === $this->container) {
+            $client = static::createClient();
+            $this->container = $client->getContainer();
+        }
+
+        return $this->container;
     }
 
 }
