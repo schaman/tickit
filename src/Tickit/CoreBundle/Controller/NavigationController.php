be386a1af218789be706dc6665aa30ee4e3ba05c
James Halsall <james.t.halsall@googlemail.com>
2012-12-01 14:46:43 +0000

    Adding navigation components

---
+++ src/Tickit/CoreBundle/Controller/NavigationController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tickit\CoreBundle\Controller;
+
+/**
+ * Provides actions related to the application navigation
+ *
+ * @author James Halsall <james.t.halsall@googlemail.com>
+ */
+class NavigationController extends CoreController
+{
+    /**
+     * Renders the top navigation bar for the application based on the current user type
+     *
+     * @return string
+     */
+    public function topNavigationAction()
+    {
+        /** @var \Symfony\Component\Routing\Router $router */
+        $router = $this->get('router');
+
+        $items = array(
+            array(
+                'name' => 'Dashboard',
+                'route' => $router->generate('dashboard_index')
+            ),
+            array(
+                'name' => 'Tickets',
+                'route' => '#'
+            ),
+            array(
+                'name' => 'Teams',
+                'route' => $router->generate('team_index')
+            ),
+            array(
+                'name' => 'Users',
+                'route' => $router->generate('user_index')
+            ),
+            array(
+                'name' => 'System',
+                'route' => '#'
+            )
+        );
+
+        return $this->render('TickitCoreBundle:Navigation:top-navigation.html.twig', array('navigation' => $items));
+    }
+
+}
