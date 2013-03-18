05968a649f30b1157bd0b63721f8bec42ddd7465
James Halsall <james.t.halsall@googlemail.com>
2013-03-18 21:16:34 +0000

    Adding manager service test

---
+++ src/Tickit/ProjectBundle/Tests/Manager/ProjectManagerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tickit\ProjectBundle\Tests\Manager;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Tests for the project manager
+ *
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class ProjectManagerTest extends WebTestCase
+{
+    /**
+     * Test to ensure that the service exists in the container
+     *
+     * @return void
+     */
+    public function testServiceExists()
+    {
+        $container = static::createClient()->getContainer();
+        $manager = $container->get('tickit_project.manager');
+
+        $this->assertInstanceOf('\Tickit\ProjectBundle\Manager\ProjectManager', $manager);
+    }
+}
