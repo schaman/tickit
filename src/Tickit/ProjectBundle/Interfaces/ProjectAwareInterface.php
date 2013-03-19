ff6e43727c55a77818b1e52350a2e91ca793b3e7
James Halsall <james.t.halsall@googlemail.com>
2013-03-19 13:43:02 +0000

    Adding interface for project aware objects

---
+++ src/Tickit/ProjectBundle/Interfaces/ProjectAwareInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tickit\ProjectBundle\Interfaces;
+
+use Tickit\ProjectBundle\Entity\Project;
+
+/**
+ * Interface for classes that are Project aware
+ *
+ * @package Namespace\Class
+ * @author  James Halsall <jhalsall@rippleffect.com>
+ * @see     Tickit\ProjectBundle\Entity\Project
+ */
+interface ProjectAwareInterface
+{
+    /**
+     * Gets the project on this object
+     *
+     * @return Project
+     */
+    function getProject();
+
+    /**
+     * Sets the project on this object
+     *
+     * @param Project $project The project to set
+     *
+     * @return mixed
+     */
+    function setProject(Project $project);
+}
