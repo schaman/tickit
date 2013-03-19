7b0a1ed657426e8410df4eee344f98005eed4031
James Halsall <james.t.halsall@googlemail.com>
2013-03-19 13:55:37 +0000

    Adding persist action and event in project manager

---
+++ src/Tickit/ProjectBundle/Event/ProjectCreatedEvent.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tickit\ProjectBundle\Event;
+
+use Tickit\ProjectBundle\Entity\Project;
+use Tickit\ProjectBundle\Interfaces\ProjectAwareInterface;
+
+/**
+ * Event dispatched when a project is created
+ *
+ * Event name: "tickit_project.event.create"
+ *
+ * @package Tickit\ProjectBundle\Event
+ * @author  James Halsall <jhalsall@rippleffect.com>
+ */
+class ProjectCreatedEvent implements ProjectAwareInterface
+{
+    /**
+     * The project that has been created
+     *
+     * @var Project
+     */
+    protected $project;
+
+    /**
+     * Constructor.
+     *
+     * @param Project $project The project that is being created
+     */
+    public function __construct(Project $project)
+    {
+        $this->project = null;
+    }
+
+    /**
+     * Gets the project on this object
+     *
+     * @return Project
+     */
+    public function getProject()
+    {
+        return $this->project;
+    }
+
+    /**
+     * Sets the project on this object
+     *
+     * @param Project $project The project to set
+     *
+     * @return mixed
+     */
+    public function setProject(Project $project)
+    {
+        $this->project = $project;
+    }
+}
--- src/Tickit/ProjectBundle/Manager/ProjectManager.php
+++ src/Tickit/ProjectBundle/Manager/ProjectManager.php
@@ -4,6 +4,10 @@ namespace Tickit\ProjectBundle\Manager;
 
 use Doctrine\Bundle\DoctrineBundle\Registry;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\EventDispatcher\ContainerAwareEventDispatcher;
+use Tickit\ProjectBundle\Entity\Project;
+use Tickit\ProjectBundle\Event\ProjectCreatedEvent;
+use Tickit\ProjectBundle\TickitProjectEvents;
 
 /**
  * Project Manager
@@ -24,12 +28,41 @@ class ProjectManager
     protected $em;
 
     /**
+     * The event dispatcher
+     *
+     * @var ContainerAwareEventDispatcher
+     */
+    protected $dispatcher;
+
+    /**
      * Constructor.
      *
-     * @param Registry $doctrine The doctrine registry service
+     * @param Registry                      $doctrine   The doctrine registry service
+     * @param ContainerAwareEventDispatcher $dispatcher The event dispatcher
      */
-    public function __construct(Registry $doctrine)
+    public function __construct(Registry $doctrine, ContainerAwareEventDispatcher $dispatcher)
     {
         $this->em = $doctrine->getManager();
+        $this->dispatcher = $dispatcher;
+    }
+
+    /**
+     * Persists a project entity to the database
+     *
+     * @param Project $project The project to persist
+     * @param boolean $flush   True to automatically flush changes to the database, false otherwise (defaults to true)
+     *
+     * @return void
+     */
+    public function persist(Project $project, $flush = true)
+    {
+        $this->em->persist($project);
+
+        if (false !== $flush) {
+            $this->em->flush();
+        }
+
+        $event = new ProjectCreatedEvent($project);
+        $this->dispatcher->dispatch(TickitProjectEvents::PROJECT_CREATE, $event);
     }
 }
