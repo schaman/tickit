461dc607ee36d1e1179c9293493a94fc1b4beaed
James Halsall <james.t.halsall@googlemail.com>
2013-03-20 13:58:53 +0000

    Refactoring tests for AbstractBeforeDeleteEvent --> AbstractVetoableEvent

--- src/Tickit/CoreBundle/Tests/Event/AbstractVetoableEventTest.php
+++ src/Tickit/CoreBundle/Tests/Event/AbstractVetoableEventTest.php
@@ -2,24 +2,24 @@
 
 namespace Tickit\CoreBundle\Tests\Event;
 
-use Tickit\CoreBundle\Event\AbstractBeforeDeleteEvent;
+use Tickit\CoreBundle\Event\AbstractVetoableEvent;
 
 /**
- * Tests for the AbstractBeforeDeleteEvent object
+ * Tests for the AbstractVetoableEvent object
  *
  * @package Tickit\CoreBundle\Tests\Event
  * @author  James Halsall <james.t.halsall@googlemail.com>
  */
-class AbstractBeforeDeleteEventTest extends \PHPUnit_Framework_TestCase
+class AbstractVetoableEventTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * Ensures that the AbstractBeforeDeleteEvent object is a descendant of the event class
+     * Ensures that the AbstractVetoableEvent object is a descendant of the event class
      *
      * @return void
      */
     public function testAbstractIsAnEvent()
     {
-        $event = $this->getMockForAbstractClass('\Tickit\CoreBundle\Event\AbstractBeforeDeleteEvent');
+        $event = $this->getMockForAbstractClass('\Tickit\CoreBundle\Event\AbstractVetoableEvent');
         $this->assertInstanceOf('Symfony\Component\EventDispatcher\Event', $event);
     }
 
@@ -32,8 +32,8 @@ class AbstractBeforeDeleteEventTest extends \PHPUnit_Framework_TestCase
      */
     public function testIsVetoedReturnsCorrectly()
     {
-        /** @var AbstractBeforeDeleteEvent $event  */
-        $event = $this->getMockForAbstractClass('\Tickit\CoreBundle\Event\AbstractBeforeDeleteEvent');
+        /** @var AbstractVetoableEvent $event  */
+        $event = $this->getMockForAbstractClass('\Tickit\CoreBundle\Event\AbstractVetoableEvent');
 
         $this->assertFalse($event->isVetoed(), 'isVeoted() correctly returns false initially');
         $event->veto();
