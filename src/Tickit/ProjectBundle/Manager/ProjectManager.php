a4cdb5704ee32023c03f71b1f5e3321fa390acb0
James Halsall <james.t.halsall@googlemail.com>
2013-03-20 12:57:51 +0000

    Adding update method in ProjectManager

--- src/Tickit/ProjectBundle/Manager/ProjectManager.php
+++ src/Tickit/ProjectBundle/Manager/ProjectManager.php
@@ -6,8 +6,10 @@ use Tickit\CoreBundle\Manager\AbstractManager;
 use Tickit\ProjectBundle\Entity\Project;
 use Tickit\ProjectBundle\Event\BeforeCreateEvent;
 use Tickit\ProjectBundle\Event\BeforeDeleteEvent;
+use Tickit\ProjectBundle\Event\BeforeUpdateEvent;
 use Tickit\ProjectBundle\Event\CreateEvent;
 use Tickit\ProjectBundle\Event\DeleteEvent;
+use Tickit\ProjectBundle\Event\UpdateEvent;
 use Tickit\ProjectBundle\TickitProjectEvents;
 
 /**
@@ -24,6 +26,8 @@ class ProjectManager extends AbstractManager
     /**
      * Creates a project by persisting it in the entity manager
      *
+     * Will also dispatch the "tickit_project.event.before_create" and "tickit_project.event.create" events
+     *
      * @param Project $project The project to create in the entity manager
      * @param boolean $flush   True to automatically flush changes to the database, false otherwise (defaults to true)
      *
@@ -34,12 +38,37 @@ class ProjectManager extends AbstractManager
         $beforeEvent = new BeforeCreateEvent($project);
         /** @var BeforeCreateEvent $beforeEvent  */
         $beforeEvent = $this->dispatcher->dispatch(TickitProjectEvents::PROJECT_BEFORE_CREATE, $beforeEvent);
-        $project = $beforeEvent->getProject(); // a subscriber may have updated the project so we re-fetch it from the event
 
+        // a subscriber may have updated the project so we re-fetch it from the event
+        $project = $beforeEvent->getProject();
         $this->internalPersist($project, $flush);
 
         $event = new CreateEvent($project);
         $this->dispatcher->dispatch(TickitProjectEvents::PROJECT_CREATE, $event);
+    }
+
+    /**
+     * Updates a project by flushing changes to the entity manager.
+     *
+     * Will also dispatch the "tickit_project.event.before_update" and "tickit_project.event.update" events
+     *
+     * @param Project $project The project to create in the entity manager
+     * @param boolean $flush   True to automatically flush changes to the database, false otherwise (defaults to true)
+     *
+     * @return void
+     */
+    public function update(Project $project, $flush = true)
+    {
+        $beforeEvent = new BeforeUpdateEvent($project);
+        /** @var BeforeUpdateEvent $beforeEvent */
+        $beforeEvent = $this->dispatcher->dispatch(TickitProjectEvents::PROJECT_BEFORE_UPDATE, $beforeEvent);
+
+        // a subscriber may have updated the project so we re-fetch it from the event
+        $project = $beforeEvent->getProject();
+        $this->internalPersist($project, $flush);
+
+        $event = new UpdateEvent($project);
+        $this->dispatcher->dispatch(TickitProjectEvents::PROJECT_UPDATE, $event);
     }
 
     /**
