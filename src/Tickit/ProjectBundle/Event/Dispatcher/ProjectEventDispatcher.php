7a52077f34f18d1b12d621260ca166e9dc356807
James Halsall <james.t.halsall@googlemail.com>
2013-03-21 22:06:24 +0000

    Adding ProjectEventDispatcher

---
+++ src/Tickit/ProjectBundle/Event/Dispatcher/ProjectEventDispatcher.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Tickit\ProjectBundle\Event\Dispatcher;
+
+use Tickit\CoreBundle\Entity\Interfaces\DeletableEntityInterface;
+use Tickit\CoreBundle\Event\AbstractVetoableEvent;
+use Tickit\CoreBundle\Event\Dispatcher\AbstractEntityEventDispatcher;
+use Tickit\ProjectBundle\Event\BeforeCreateEvent;
+use Tickit\ProjectBundle\Event\BeforeDeleteEvent;
+use Tickit\ProjectBundle\Event\BeforeUpdateEvent;
+use Tickit\ProjectBundle\Event\CreateEvent;
+use Tickit\ProjectBundle\Event\DeleteEvent;
+use Tickit\ProjectBundle\Event\UpdateEvent;
+use Tickit\ProjectBundle\TickitProjectEvents;
+
+/**
+ * Event dispatcher for the Project entity
+ *
+ * This class is responsible for firing events related to the management of the
+ * Project entity
+ *
+ * @package Namespace\Class
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ * @see     Tickit\ProjectBundle\Entity\Project
+ */
+class EventDispatcher extends AbstractEntityEventDispatcher
+{
+    /**
+     * Dispatches events for the "before create" event on the entity
+     *
+     * @param object $entity The project entity that is about to be created
+     *
+     * @return AbstractVetoableEvent
+     */
+    public function dispatchBeforeCreateEvent($entity)
+    {
+        $beforeEvent = new BeforeCreateEvent($entity);
+        $beforeEvent = $this->dispatcher->dispatch(TickitProjectEvents::PROJECT_BEFORE_CREATE, $beforeEvent);
+
+        return $beforeEvent;
+    }
+
+    /**
+     * Dispatches events for the "create" event on the entity
+     *
+     * @param object $entity The project entity that has just been created
+     *
+     * @return void
+     */
+    public function dispatchCreateEvent($entity)
+    {
+        $event = new CreateEvent($entity);
+        $this->dispatcher->dispatch(TickitProjectEvents::PROJECT_CREATE, $event);
+    }
+
+    /**
+     * Dispatches events for the "before update" event on the entity
+     *
+     * @param object $entity The entity that is about to be updated
+     *
+     * @return AbstractVetoableEvent
+     */
+    public function dispatchBeforeUpdateEvent($entity)
+    {
+        $beforeEvent = new BeforeUpdateEvent($entity);
+        $beforeEvent = $this->dispatcher->dispatch(TickitProjectEvents::PROJECT_BEFORE_UPDATE, $beforeEvent);
+
+        return $beforeEvent;
+    }
+
+    /**
+     * Dispatches events for the "update" event on the entity
+     *
+     * @param object $entity         The entity that has just been updated
+     * @param object $originalEntity The entity before any changes were applied
+     *
+     * @return void
+     */
+    public function dispatchUpdateEvent($entity, $originalEntity)
+    {
+        $event = new UpdateEvent($entity, $originalEntity);
+        $this->dispatcher->dispatch(TickitProjectEvents::PROJECT_UPDATE, $event);
+    }
+
+    /**
+     * Dispatches events for the "before delete" event on the entity
+     *
+     * @param DeletableEntityInterface $entity The entity that is about to be deleted
+     *
+     * @return AbstractVetoableEvent
+     */
+    public function dispatchBeforeDeleteEvent(DeletableEntityInterface $entity)
+    {
+        $beforeEvent = new BeforeDeleteEvent($entity);
+        $beforeEvent = $this->dispatcher->dispatch(TickitProjectEvents::PROJECT_BEFORE_DELETE, $beforeEvent);
+
+        return $beforeEvent;
+    }
+
+    /**
+     * Dispatches events for the "delete" event on the entity
+     *
+     * @param DeletableEntityInterface $entity The entity that has just been deleted in the entity manager
+     *
+     * @return void
+     */
+    public function dispatchDeleteEvent(DeletableEntityInterface $entity)
+    {
+        $event = new DeleteEvent($entity);
+        $this->dispatcher->dispatch(TickitProjectEvents::PROJECT_DELETE, $event);
+    }
+}
