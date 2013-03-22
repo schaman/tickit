1ad12e43590ac85b61fbd1d44989fe77dc0532bc
James Halsall <james.t.halsall@googlemail.com>
2013-03-22 22:01:23 +0000

    Adding EntityAwareEventInterface

---
+++ src/Tickit/CoreBundle/Event/Interfaces/EntityAwareEventInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tickit\CoreBundle\Event\Interfaces;
+
+/**
+ * Interface for entity aware events.
+ *
+ * Any events implementing this interface are guaranteeing that they are
+ * responsible for an entity. This proves useful when an object is responsible
+ * for the management of events when it does not have insightful details about
+ * the entities on those events.
+ *
+ * @package Tickit\CoreBundle\Event\Interfaces
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ * @see     Tickit\CoreBundle\Manager\AbstractManager
+ */
+interface EntityAwareInterface
+{
+    /**
+     * Gets the entity associated with this event
+     *
+     * @return object
+     */
+    function getEntity();
+
+    /**
+     * Sets the entity associated with this event
+     *
+     * @param object $entity The entity to attach to the event
+     */
+    function setEntity($entity);
+}
