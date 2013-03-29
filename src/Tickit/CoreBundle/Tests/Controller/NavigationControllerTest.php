bd6cb25567e84b1eb670ff8cfb55a3cd24da1ee6
James Halsall <james.t.halsall@googlemail.com>
2013-03-29 15:52:34 +0000

    Adding test specific routing file

---
+++ src/Tickit/CoreBundle/Tests/Controller/NavigationControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tickit\CoreBundle\Tests\Controller;
+
+/**
+ * NavigationController tests.
+ *
+ * @package Tickit\CoreBundle\Tests\Controller
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class NavigationController
+{
+
+}
