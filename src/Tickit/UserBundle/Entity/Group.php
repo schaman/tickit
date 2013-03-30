44b3c04975bb4dc911f509e696c4d6c3a65a1e26
James Halsall <james.t.halsall@googlemail.com>
2013-03-30 22:48:23 +0000

    Adding __toString() and updating phpdoc in Group entity

--- src/Tickit/UserBundle/Entity/Group.php
+++ src/Tickit/UserBundle/Entity/Group.php
@@ -17,6 +17,8 @@ use Doctrine\ORM\Mapping as ORM;
 class Group extends BaseGroup
 {
     /**
+     * The unique identifier for this group
+     *
      * @ORM\Id
      * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
@@ -24,7 +26,19 @@ class Group extends BaseGroup
     protected $id;
 
     /**
+     * Permissions associated with this group
+     *
      * @ORM\OneToMany(targetEntity="Tickit\PermissionBundle\Entity\GroupPermissionValue", mappedBy="group")
      */
     protected $permissions;
+
+    /**
+     * __toString method
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return (string) $this->getName();
+    }
 }
