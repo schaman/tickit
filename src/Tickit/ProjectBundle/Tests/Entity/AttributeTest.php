be4fe0c49a358080e167d59b2bddfece398758c8
James Halsall <james.t.halsall@googlemail.com>
2013-04-09 13:32:41 +0100

    Adding additional tests for Attribute entity in project bundle

--- src/Tickit/ProjectBundle/Tests/Entity/AttributeTest.php
+++ src/Tickit/ProjectBundle/Tests/Entity/AttributeTest.php
@@ -38,4 +38,48 @@ class AttributeTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals(Attribute::TYPE_CHOICE, $attribute->getType());
     }
+
+
+    /**
+     * Tests the setMetaData() method
+     *
+     * @expectedException \InvalidArgumentException
+     *
+     * @return void
+     */
+    public function testSetMetaDataThrowsExceptionForInvalidArgument()
+    {
+        $attribute = new Attribute();
+        $attribute->setMetaDeta('blah');
+    }
+
+    /**
+     * Tests the setMetaData() method
+     *
+     * @return void
+     */
+    public function testSetMetaDataAcceptsJsonEncodedString()
+    {
+        $attribute = new Attribute();
+        $attribute->setMetaDeta(json_encode(array('1' => 2)));
+
+        $this->assertEquals(json_encode(array('1' => 2)), $attribute->getMetaDeta());
+    }
+
+    /**
+     * Tests the getMetaData() method
+     *
+     * @return void
+     */
+    public function testGetMetaDataReturnsDecodedDataCorrectly()
+    {
+        $object = new \stdClass();
+        $object->property = 1;
+        $object->something = 'some string';
+
+        $attribute = new Attribute();
+        $attribute->setMetaDeta(json_encode($object));
+
+        $this->assertEquals($object, $attribute->getMetaDeta(true));
+    }
 }
