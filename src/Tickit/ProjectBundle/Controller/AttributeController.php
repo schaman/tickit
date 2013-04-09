25a440f8f1913e65aec60c3bcb6b2fd3bca1d7da
James Halsall <james.t.halsall@googlemail.com>
2013-04-09 19:38:01 +0100

    Adding attribute controller, routes and literal attribute form

---
+++ src/Tickit/ProjectBundle/Controller/AttributeController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tickit\ProjectBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Tickit\CoreBundle\Controller\AbstractCoreController;
+use Tickit\ProjectBundle\Entity\Attribute;
+use Tickit\ProjectBundle\Form\Type\LiteralAttributeFormType;
+
+
+/**
+ * Project attribute controller.
+ *
+ * Responsible for handling requests related to project attributes
+ *
+ * @package Tickit\ProjectBundle\Controller
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class AttributeController extends AbstractCoreController
+{
+    /**
+     * Create attribute action.
+     *
+     * Displays relevant form for creating a project attribute of the given type.
+     *
+     * @param string $type The type of the attribute to create
+     *
+     * @Template("TickitProjectBundle:Project:edit.html.twig")
+     *
+     * @throws NotFoundHttpException If the given type is not valid
+     *
+     * @return array
+     */
+    public function createAction($type)
+    {
+        $availableTypes = Attribute::getAvailableTypes();
+
+        if (!in_array($type, $availableTypes)) {
+            throw $this->createNotFoundException('An invalid attribute type was specified');
+        }
+
+        switch ($type) {
+            case Attribute::TYPE_CHOICE:
+                break;
+            case Attribute::TYPE_ENTITY:
+                break;
+            case Attribute::TYPE_TEXT:
+                $formType = new LiteralAttributeFormType();
+                break;
+        }
+
+        $attribute = new Attribute();
+        $form = $this->createForm($formType, $attribute);
+
+        return array('form' => $form->createView());
+    }
+}
