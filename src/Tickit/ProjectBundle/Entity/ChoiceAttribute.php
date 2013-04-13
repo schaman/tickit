2f99f3e234a79cd254f332cb0b8782202c329d87
James Halsall <james.t.halsall@googlemail.com>
2013-04-13 20:38:41 +0100

    Adding ChoiceAttributeChoice entity, updating ChoiceAttribute

--- src/Tickit/ProjectBundle/Entity/ChoiceAttribute.php
+++ src/Tickit/ProjectBundle/Entity/ChoiceAttribute.php
@@ -2,6 +2,7 @@
 
 namespace Tickit\ProjectBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -17,6 +18,23 @@ use Doctrine\ORM\Mapping as ORM;
 class ChoiceAttribute extends AbstractAttribute
 {
     /**
+     * Choices that are associated with this choice attribute
+     *
+     * @var ArrayCollection
+     * @ORM\OneToMany(targetEntity="ChoiceAttributeChoice", mappedBy="attribute")
+     */
+    protected $choices;
+
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->choices = new ArrayCollection();
+    }
+
+    /**
      * Gets the attribute type
      *
      * @return string
@@ -24,5 +42,25 @@ class ChoiceAttribute extends AbstractAttribute
     public function getType()
     {
         return static::TYPE_CHOICE;
+    }
+
+    /**
+     * Sets the choices available on this choice attribute
+     *
+     * @param ArrayCollection $choices The new collection of choices
+     */
+    public function setChoices(ArrayCollection $choices)
+    {
+        $this->choices = $choices;
+    }
+
+    /**
+     * Gets available choices
+     *
+     * @return ArrayCollection
+     */
+    public function getChoices()
+    {
+        return $this->choices;
     }
 }
