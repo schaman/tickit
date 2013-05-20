d0d97721d9981d7945410713236e442932120be5
James Halsall <james.t.halsall@googlemail.com>
2013-05-20 21:59:29 +0100

    Starting work on PermissionEvaluator

---
+++ src/Tickit/PermissionBundle/Evaluator/PermissionEvaluator.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tickit\PermissionBundle\Evaluator;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+/**
+ * Permission evaluator.
+ *
+ * Evaluates permissions for the currently logged in user.
+ *
+ * @package Tickit\PermissionBundle\Evaluator
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class PermissionEvaluator implements EvaluatorInterface
+{
+    /**
+     * The current session object
+     *
+     * @var SessionInterface
+     */
+    protected $session;
+
+    /**
+     * Constructor.
+     *
+     * @param SessionInterface $session The current session.
+     */
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
+    /**
+     * Evaluates whether the current user has a permission
+     *
+     * @param string $systemName The system name of the permission that is being evaluated
+     *
+     * @return boolean
+     */
+    public function hasPermission($systemName)
+    {
+        // TODO: Implement hasPermission() method.
+    }
+}
