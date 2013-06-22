b849e65a75725fda306f890ff5a1d830e55e8227
James Halsall <james.t.halsall@googlemail.com>
2013-06-22 16:26:34 +0100

    Adding ApiController for UserBundle with fetch method, adding test

---
+++ src/Tickit/UserBundle/Controller/ApiController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tickit\UserBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Tickit\UserBundle\Entity\User;
+
+/**
+ * API controller for users.
+ *
+ * Serves user content and handles user related operations via API actions.
+ *
+ * @package Tickit\UserBundle\Controller
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class ApiController extends Controller
+{
+    /**
+     * Fetches data for a particular user and serves as JSON
+     *
+     * @param User $user The user to fetch data for
+     *
+     * @ParamConverter("user", class="TickitUserBundle:User")
+     *
+     * @return JsonResponse
+     */
+    public function fetchAction(User $user = null)
+    {
+        if (null === $user) {
+            $user = $this->getUser();
+        }
+
+        $data = array(
+            'id' => $user->getId(),
+            'username' => $user->getUsername(),
+            'email' => $user->getEmail(),
+            'forename' => $user->getForename(),
+            'surname' => $user->getSurname(),
+            'avatarIdentifier' => $user->getAvatarIdentifier()
+        );
+
+        return new JsonResponse($data);
+    }
+}
---
+++ src/Tickit/UserBundle/Tests/Controller/ApiControllerTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tickit\UserBundle\Tests\Controller;
+
+use Tickit\CoreBundle\Tests\AbstractFunctionalTest;
+
+/**
+ * API Controller tests.
+ *
+ * @package Tickit\UserBundle\Tests\Controller
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class ApiControllerTest extends AbstractFunctionalTest
+{
+    /**
+     * Tests the fetchAction() method
+     *
+     * @return void
+     */
+    public function testFetchActionReturnsCorrectUserDetailsForValidId()
+    {
+        $client = $this->getAuthenticatedClient(static::$admin);
+        $requestedUser = static::$developer;
+
+        $client->request('get', $this->generateRoute('api_user_fetch', array('id' => $requestedUser->getId())));
+
+        $response = json_decode($client->getResponse()->getContent());
+        $this->assertInstanceOf('\stdClass', $response);
+        $this->assertEquals($requestedUser->getId(), $response->id);
+        $this->assertEquals($requestedUser->getUsername(), $response->username);
+        $this->assertEquals($requestedUser->getEmail(), $response->email);
+        $this->assertEquals($requestedUser->getForename(), $response->forename);
+        $this->assertEquals($requestedUser->getSurname(), $response->surname);
+        $this->assertEquals($requestedUser->getAvatarIdentifier(), $response->avatarIdentifier);
+    }
+
+    /**
+     * Tests the fetchAction()
+     *
+     * @return void
+     */
+    public function testFetchActionReturnsCurrentlyLoggedInUserWhenInvalidIdIsProvided()
+    {
+        $client = $this->getAuthenticatedClient(static::$developer);
+        $client->request('get', $this->generateRoute('api_user_fetch', array('id' => 3489689208)));
+
+        $response = json_decode($client->getResponse()->getContent());
+        $this->assertInstanceOf('\stdClass', $response);
+        $this->assertEquals(static::$developer->getId(), $response->id);
+        $this->assertEquals(static::$developer->getUsername(), $response->username);
+        $this->assertEquals(static::$developer->getEmail(), $response->email);
+        $this->assertEquals(static::$developer->getForename(), $response->forename);
+        $this->assertEquals(static::$developer->getSurname(), $response->surname);
+        $this->assertEquals(static::$developer->getAvatarIdentifier(), $response->avatarIdentifier);
+    }
+
+    /**
+     * Tests the fetchAction()
+     *
+     * @return void
+     */
+    public function testFetchActionReturnsCurrentlyLoggedInUserWhenNoIdIsProvided()
+    {
+        $client = $this->getAuthenticatedClient(static::$developer);
+        $client->request('get', $this->generateRoute('api_user_fetch'));
+
+        $response = json_decode($client->getResponse()->getContent());
+        $this->assertInstanceOf('\stdClass', $response);
+        $this->assertEquals(static::$developer->getId(), $response->id);
+        $this->assertEquals(static::$developer->getUsername(), $response->username);
+        $this->assertEquals(static::$developer->getEmail(), $response->email);
+        $this->assertEquals(static::$developer->getForename(), $response->forename);
+        $this->assertEquals(static::$developer->getSurname(), $response->surname);
+        $this->assertEquals(static::$developer->getAvatarIdentifier(), $response->avatarIdentifier);
+    }
+}
