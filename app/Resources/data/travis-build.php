3265d082c4ea1f476f8a4aa98567fdcc8d6bc703
James Halsall <james.t.halsall@googlemail.com>
2013-07-03 14:42:26 +0100

    Updating travis build so parameters.yml is created from parameters.yml.dit, updating dependencies

--- app/Resources/data/travis-build.php
+++ app/Resources/data/travis-build.php
@@ -23,28 +23,8 @@ function getProjectRoot()
  */
 function createTravisConfigYml()
 {
-    $fileContent = <<<CONTENT
-parameters:
-    database_driver:   pdo_mysql
-    database_host:     localhost
-    database_port:     ~
-    database_name:     tickit
-    database_user:     root
-    database_password:
-
-    test_database_user: user
-
-    mailer_transport:  smtp
-    mailer_host:       localhost
-    mailer_user:       ~
-    mailer_password:   ~
-
-    locale:            en
-    secret:            SomeRandomValue
-CONTENT;
-
     $configDir = getProjectRoot() . '/app/config';
-    file_put_contents($configDir . '/parameters.yml', $fileContent);
+    file_put_contents($configDir . '/parameters.yml', file_get_contents($configDir . '/parameters.yml.dist'));
 
 }
 
