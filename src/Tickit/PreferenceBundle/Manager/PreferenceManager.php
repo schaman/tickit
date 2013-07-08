9a84c4a03a1347995663cd0deb663f3f8f8d13d1
James Halsall <james.t.halsall@googlemail.com>
2013-07-08 18:26:06 +0100

    Adding PreferenceManager

---
+++ src/Tickit/PreferenceBundle/Manager/PreferenceManager.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tickit\PreferenceBundle\Manager;
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Tickit\PreferenceBundle\Entity\Repository\PreferenceRepository;
+
+/**
+ * Preference Manager.
+ *
+ * Provides functionality for managing preference data in the application.
+ *
+ * @package Tickit\PreferenceBundle\Manager
+ * @author  James Halsall <jhalsall@rippleffect.com>
+ */
+class PreferenceManager
+{
+    /**
+     * The doctrine registry
+     *
+     * @var Registry
+     */
+    protected $doctrine;
+
+    /**
+     * Constructor
+     *
+     * @param Registry $doctrine The doctrine registry
+     */
+    public function __construct(Registry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    /**
+     * Gets the preferences repository
+     *
+     * @return PreferenceRepository
+     */
+    public function getRepository()
+    {
+        return $this->doctrine->getRepository('TickitPreferenceBundle:Preference');
+    }
+}
