4f18b3f1073e3c22bbb832384316d775272dc81f
James Halsall <james.t.halsall@googlemail.com>
2013-07-08 20:45:04 +0100

    Adding more tests for UserProvider

--- src/Tickit/UserBundle/Tests/Security/UserProviderTest.php
+++ src/Tickit/UserBundle/Tests/Security/UserProviderTest.php
@@ -4,6 +4,7 @@ namespace Tickit\UserBundle\Tests\Security;
 
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Tickit\CoreBundle\Tests\AbstractFunctionalTest;
+use Tickit\UserBundle\Entity\User;
 use Tickit\UserBundle\Security\UserProvider;
 use Tickit\UserBundle\Tests\Security\Mock\MockInvalidTypeUser;
 
@@ -41,6 +42,19 @@ class UserProviderTest extends AbstractFunctionalTest
     }
 
     /**
+     * Tests the loadByUsername() method
+     *
+     * @expectedException \Symfony\Component\Security\Core\Exception\UsernameNotFoundException
+     *
+     * @return void
+     */
+    public function testLoadByUsernameThrowsExceptionForNonExistentUser()
+    {
+        $provider = $this->getProvider();
+        $provider->loadUserByUsername('something that is not valid');
+    }
+
+    /**
      * Tests the refreshUser() method
      *
      * @expectedException \Symfony\Component\Security\Core\Exception\UnsupportedUserException
@@ -51,6 +65,21 @@ class UserProviderTest extends AbstractFunctionalTest
     {
         $provider = $this->getProvider();
         $provider->refreshUser(new MockInvalidTypeUser());
+    }
+
+    /**
+     * Tests the refreshUser() method
+     *
+     * @expectedException \Symfony\Component\Security\Core\Exception\UsernameNotFoundException
+     *
+     * @return void
+     */
+    public function testRefreshUserThrowsExceptionForNonExistentUser()
+    {
+        $user = new User();
+
+        $provider = $this->getProvider();
+        $provider->refreshUser($user);
     }
 
     /**
