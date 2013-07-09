08b1952968201826663e7d9d50fed7d951f0cf00
James Halsall <james.t.halsall@googlemail.com>
2013-07-09 08:19:21 +0100

    Removing unused methods in core controller

--- src/Tickit/CoreBundle/Controller/AbstractCoreController.php
+++ src/Tickit/CoreBundle/Controller/AbstractCoreController.php
@@ -20,41 +20,6 @@ use Tickit\CacheBundle\Cache\CacheFactory;
 abstract class AbstractCoreController extends Controller
 {
     /**
-     * Returns an instance of the user manager
-     *
-     * @return UserManager
-     */
-    protected function getUserManager()
-    {
-        return $this->container->get('tickit_user.manager');
-    }
-
-    /**
-     * Returns an instance of the currently logged in user. If no user is logged in
-     * this function will return null
-     *
-     * @return User
-     */
-    protected function getCurrentUser()
-    {
-        $token = $this->get('security.context')->getToken();
-
-        return (null !== $token) ? $token->getUser() : null;
-    }
-
-    /**
-     * Gets the cache factory.
-     *
-     * Returns an instance of the caching factory which provides access to the different caching engines
-     *
-     * @return CacheFactory
-     */
-    protected function getCacheFactory()
-    {
-        return $this->get('tickit_cache.factory');
-    }
-
-    /**
      * Gets an array decorator
      *
      * @return DomainObjectArrayDecorator
