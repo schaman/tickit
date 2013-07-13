5849bc1c18eb20f4a28ba24579d533210c96c6f9
James Halsall <james.t.halsall@googlemail.com>
2013-07-13 19:32:21 +0100

    Adding team update action tests

--- src/Tickit/TeamBundle/Tests/Controller/TeamControllerTest.php
+++ src/Tickit/TeamBundle/Tests/Controller/TeamControllerTest.php
@@ -31,7 +31,7 @@ class TeamControllerTest extends AbstractFunctionalTest
         $doctrine = static::createClient()->getContainer()->get('doctrine');
 
         static::$team = $doctrine->getRepository('TickitTeamBundle:Team')
-                                    ->findOneByName('Test Team 1');
+                                 ->findOneByName('Test Team 1');
     }
 
     /**
@@ -80,5 +80,65 @@ class TeamControllerTest extends AbstractFunctionalTest
 
         $team = $doctrine->getRepository('TickitTeamBundle:Team')->findOneByName($teamName);
         $this->assertInstanceOf('\Tickit\TeamBundle\Entity\Team', $team);
+    }
+
+    /**
+     * Tests the editTeamAction() method
+     *
+     * @return void
+     */
+    public function testEditTeamActionReturnsFormContentForInvalidDetails()
+    {
+        $client = $this->getAuthenticatedClient(static::$admin);
+        $doctrine = $client->getContainer()->get('doctrine');
+        $route = $this->generateRoute('team_edit_form', array('id' => static::$team->getId()));
+        $crawler = $client->request('get', $route);
+
+        $form = $crawler->selectButton('Save Changes')->form(
+            array('tickit_team[name]' => '')
+        );
+        $client->submit($form);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $response = json_decode($client->getResponse()->getContent());
+        $this->assertFalse($response->success);
+        $this->assertNotEmpty($response->form);
+
+        $nonEditedTeam = $doctrine->getRepository('TickitTeamBundle:Team')->find(static::$team->getId());
+        $this->assertEquals($nonEditedTeam->getName(), static::$team->getName());
+    }
+
+    /**
+     * Tests the editTeamAction() method
+     *
+     * @return void
+     */
+    public function testEditTeamActionUpdatesTeamForValidDetails()
+    {
+        $client = $this->getAuthenticatedClient(static::$admin);
+        $container = $client->getContainer();
+        $doctrine = $container->get('doctrine');
+
+        $newName = __FUNCTION__ . uniqid();
+        $newTeam = new Team();
+        $newTeam->setName($newName);
+        $container->get('tickit_team.manager')->create($newTeam);
+
+        $route = $this->generateRoute('team_edit_form', array('id' => $newTeam->getId()));
+        $crawler = $client->request('get', $route);
+
+        $form = $crawler->selectButton('Save Changes')->form(
+            array('tickit_team[name]' => $newName . '_updated')
+        );
+        $client->submit($form);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $response = json_decode($client->getResponse()->getContent());
+        $this->assertTrue($response->success);
+
+        $updatedTeam = $doctrine->getRepository('TickitTeamBundle:Team')->find($newTeam->getId());
+        $doctrine->getManager()->refresh($updatedTeam);
+
+        $this->assertEquals($newName . '_updated', $updatedTeam->getName());
     }
 }
