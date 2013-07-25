81fc3bf31b646ffb8fd85736ef45af265101a8fe
James Halsall <james.t.halsall@googlemail.com>
2013-07-25 20:06:51 +0100

    Updating extension file for notification bundle

--- src/Tickit/NotificationBundle/DependencyInjection/TickitNotificationExtension.php
+++ src/Tickit/NotificationBundle/DependencyInjection/TickitNotificationExtension.php
@@ -5,22 +5,27 @@ namespace Tickit\NotificationBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 /**
- * todo
+ * Tickit notification bundle DI extension.
+ *
+ * @package Tickit\NotificationBundle\DependencyInjection
+ * @author  James Halsall <james.t.halsall@googlemail.com>
  */
 class TickitNotificationExtension extends Extension
 {
     /**
      * {@inheritDoc}
+     *
+     * @return void
      */
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
 }
