40ca0e0ee98fb6e5e33a21356d8fc93893829315
James Halsall <james.t.halsall@googlemail.com>
2013-07-27 08:47:53 +0100

    Adding notification provider

---
+++ src/Tickit/NotificationBundle/Provider/NotificationProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tickit\NotificationBundle\Provider;
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Tickit\UserBundle\Entity\User;
+
+/**
+ * Notification provider.
+ *
+ * Responsible for providing notification data in the application.
+ *
+ * @package Tickit\NotificationBundle\Provider
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class NotificationProvider
+{
+    /**
+     * The doctrine registry
+     *
+     * @var Registry
+     */
+    protected $doctrine;
+
+    /**
+     * The maximum number of notification messages to return.
+     *
+     * @var integer
+     */
+    protected $messageLimit;
+
+    /**
+     * Constructor.
+     *
+     * @param Registry $doctrine     The doctrine registry
+     * @param integer  $messageLimit The maximum number of notification messages to return
+     */
+    public function __construct(Registry $doctrine, $messageLimit)
+    {
+        $this->doctrine = $doctrine;
+        $this->messageLimit = $messageLimit;
+    }
+
+    /**
+     * Finds unread notifications for a given user
+     *
+     * @param User $user The user to find notifications for
+     *
+     * @return void
+     */
+    public function findUnreadForUser(User $user)
+    {
+        //todo
+    }
+}
