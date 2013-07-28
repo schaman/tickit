139d4e5848af2e2066365bcbcbbf2d9b85716dfc
James Halsall <james.t.halsall@googlemail.com>
2013-07-28 18:33:32 +0100

    Adding test to ensure that group notification logic behaves as expected

--- src/Tickit/NotificationBundle/Tests/Controller/ApiControllerTest.php
+++ src/Tickit/NotificationBundle/Tests/Controller/ApiControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tickit\NotificationBundle\Tests\Controller;
 
 use Tickit\CoreBundle\Tests\AbstractFunctionalTest;
+use Tickit\NotificationBundle\Entity\GroupNotification;
 use Tickit\NotificationBundle\Entity\UserNotification;
 
 /**
@@ -85,6 +86,42 @@ class ApiControllerTest extends AbstractFunctionalTest
 
         // cleanup
         $em->remove($userNotification);
+        $em->flush();
+    }
+
+    /**
+     * Tests the listAction() method
+     *
+     * @return void
+     */
+    public function testListActionShowsGroupNotifications()
+    {
+        $client = $this->getAuthenticatedClient(static::$developer);
+        $doctrine = $client->getContainer()->get('doctrine');
+        $em = $doctrine->getManager();
+        $user = $em->getRepository('TickitUserBundle:User')->find(static::$developer->getId());
+
+        $groupNotification = new GroupNotification();
+        $groupNotification->setRecipient($user->getGroup())
+                          ->setMessage('test')
+                          ->setActionUri('/');
+
+        $em->persist($groupNotification);
+        $em->flush();
+
+        $route = $this->generateRoute('api_notification_list');
+        $client->request('get', $route);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $response = json_decode($client->getResponse()->getContent());
+        $this->assertInternalType('array', $response);
+        $this->assertNotEmpty($response);
+
+        $notification = $response[0];
+        $this->assertEquals($groupNotification->getMessage(), $notification->message);
+        $this->assertEquals($groupNotification->getActionUri(), $notification->actionUri);
+
+        // cleanup
+        $em->remove($groupNotification);
         $em->flush();
     }
 }
