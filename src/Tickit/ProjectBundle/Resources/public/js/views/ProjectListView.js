8863d92412540d688cc1a3943b6340b44405b19c
James Halsall <james.t.halsall@googlemail.com>
2013-07-29 21:50:04 +0100

    Adding click event handler for links in the project list

--- src/Tickit/ProjectBundle/Resources/public/js/views/ProjectListView.js
+++ src/Tickit/ProjectBundle/Resources/public/js/views/ProjectListView.js
@@ -17,6 +17,19 @@ define([
         template: '#project_list-template',
         itemView: rowView,
 
+        events: {
+            "click a": "linkClick"
+        },
+
+        /**
+         * Handles a click event on an <a> tag
+         *
+         */
+        linkClick : function(e) {
+            e.preventDefault();
+            App.Router.goTo($(e.target).attr('href'));
+        },
+
         appendHtml: function(collectionView, itemView) {
             collectionView.$('tbody').append(itemView.el);
         }
