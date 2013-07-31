95da32de33a04712ea0db257b4699e028fdf12a1
James Halsall <james.t.halsall@googlemail.com>
2013-07-31 21:26:15 +0100

    Completing implementation of KernelExceptionListener for returning correct 403 response codes when not logged in

--- src/Tickit/UserBundle/Listener/KernelExceptionListener.php
+++ src/Tickit/UserBundle/Listener/KernelExceptionListener.php
@@ -4,22 +4,35 @@ namespace Tickit\UserBundle\Listener;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 /**
- * File description
+ * Kernel exception listener.
  *
- * @package Namespace\Class
- * @author  James Halsall <jhalsall@rippleffect.com>
+ * Handles exceptions thrown within the kernel.
+ *
+ * @package Tickit\UserBundle\Listener
+ * @author  James Halsall <james.t.halsall@googlemail.com>
  */
 class KernelExceptionListener
 {
     /**
+     * Handles a kernel exception.
      *
+     * If the request is an XmlHttpRequest and an AccessDeniedException has been thrown then
+     * this method will set the response to a 403 rather than redirecting to /login.
      *
-     * @param GetResponseForExceptionEvent $event
+     * @param GetResponseForExceptionEvent $event The exception event
+     *
+     * @return void
      */
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
-        $event->setResponse(new Response('', 403));
+        $request = $event->getRequest();
+        $exception = $event->getException();
+
+        if ($request->isXmlHttpRequest() && $exception instanceof AccessDeniedException) {
+            $event->setResponse(new Response('', 403));
+        }
     }
 }
