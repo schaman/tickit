9b5f5167a2235091c4dc3d05f9309c5363cf9680
James Halsall <james.t.halsall@googlemail.com>
2013-09-16 19:45:34 +0100

    Removing unused method in UserRepository

--- src/Tickit/UserBundle/Entity/Repository/UserRepository.php
+++ src/Tickit/UserBundle/Entity/Repository/UserRepository.php
@@ -21,35 +21,6 @@ class UserRepository extends EntityRepository implements FilterableRepositoryInt
     const COLUMN_USERNAME = 'username';
 
     /**
-     * Finds a user that has been active in the last X minutes (determined by the $minutes parameter)
-     *
-     * @param string $sessionToken The session ID of the user to find
-     * @param int    $minutes      The number of maximum number of minutes since the last activity
-     *
-     * @return User
-     */
-    public function findActiveUserBySessionToken($sessionToken, $minutes = 15)
-    {
-        $seconds = $minutes * 60;
-        $lastActive = new DateTime(strtotime('Y-m-d H:i:s'));
-        $lastActive->modify("-{$seconds} seconds");
-        $lastActiveDate = $lastActive->format('Y-m-d H:i:s');
-
-        $user = $this->getEntityManager()
-                     ->createQuery(
-                         'SELECT u.id, u.username, u.email, u.session_token, u.last_activity
-                              FROM Tickit\UserBundle\Entity\User u
-                            WHERE u.last_activity >= :last_active
-                            AND u.session_token LIKE :session_token'
-                     )
-                     ->setParameter('last_active', $lastActiveDate)
-                     ->setParameter('session_token', $sessionToken)
-                     ->execute();
-
-        return $user;
-    }
-
-    /**
      * Returns a collection of users that match the given criteria
      *
      * @param array $filters An array of filters used to filter the result
