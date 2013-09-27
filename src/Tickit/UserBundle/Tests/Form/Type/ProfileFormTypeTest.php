6252c1e739c0f3f48c677dc145f9c6598dbbdf9f
James Halsall <james.t.halsall@googlemail.com>
2013-09-27 17:47:40 +0100

    Adding basic ProfileFormType test

---
+++ src/Tickit/UserBundle/Tests/Form/Type/ProfileFormTypeTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tickit\UserBundle\Tests\Form\Type;
+
+use Symfony\Component\Form\Extension\Validator\ValidatorExtension;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\Test\TypeTestCase;
+use Symfony\Component\Validator\Validation;
+use Tickit\UserBundle\Entity\User;
+use Tickit\UserBundle\Form\Type\ProfileFormType;
+
+/**
+ * ProfileFormType tests
+ *
+ * @package Tickit\UserBundle\Tests\Form\Type
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class ProfileFormTypeTest extends TypeTestCase
+{
+    /**
+     * The form under test
+     *
+     * @var ProfileFormType
+     */
+    private $formType;
+
+    /**
+     * Setup
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->formType = new ProfileFormType('Tickit\UserBundle\Entity\User');
+    }
+
+    /**
+     * Tests the form submit
+     *
+     * @return void
+     */
+    public function testSubmitValidData()
+    {
+        $user = new User();
+        $user->setForename('forename')
+             ->setSurname('surname')
+             ->setEmail('email@domain.com')
+             ->setPlainPassword('password')
+             ->setPassword('hashed-password');
+
+        $form = $this->factory->create($this->formType);
+        $form->setData($user);
+
+        $this->assertTrue($form->isSynchronized());
+        $this->assertEquals($user, $form->getData());
+
+        $expectedViewComponents = array('forename', 'surname', 'password');
+        $view = $form->createView();
+        foreach ($expectedViewComponents as $component) {
+            $this->assertArrayHasKey($component, $view->children);
+        }
+    }
+
+    /**
+     * Gets form extensions
+     *
+     * @return array
+     */
+    protected function getExtensions()
+    {
+        return array(new ValidatorExtension(Validation::createValidator()));
+    }
+}
