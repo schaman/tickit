bf89467d625606529da65bb2be1dad12ba0986f4
James Halsall <james.t.halsall@googlemail.com>
2013-09-28 10:28:01 +0100

    Adding test for ChoiceAttributeFormType

--- src/Tickit/ProjectBundle/Tests/Form/Type/ChoiceAttributeFormTypeTest.php
+++ src/Tickit/ProjectBundle/Tests/Form/Type/ChoiceAttributeFormTypeTest.php
@@ -1,15 +1,84 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: jameshalsall
- * Date: 28/09/2013
- * Time: 09:57
- * To change this template use File | Settings | File Templates.
- */
 
 namespace Tickit\ProjectBundle\Tests\Form\Type;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Form\Extension\Core\CoreExtension;
+use Symfony\Component\Form\PreloadedExtension;
+use Tickit\CoreBundle\Tests\Form\Type\AbstractFormTypeTestCase;
+use Tickit\ProjectBundle\Entity\ChoiceAttribute;
+use Tickit\ProjectBundle\Entity\ChoiceAttributeChoice;
+use Tickit\ProjectBundle\Form\Type\ChoiceAttributeChoiceFormType;
+use Tickit\ProjectBundle\Form\Type\ChoiceAttributeFormType;
 
-class ChoiceAttributeFormTypeTest extends \PHPUnit_Framework_TestCase {
+/**
+ * ChoiceAttributeFormType tests.
+ *
+ * @package Tickit\ProjectBundle\Tests\Form\Type
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class ChoiceAttributeFormTypeTest extends AbstractFormTypeTestCase
+{
+    /**
+     * Setup
+     */
+    protected function setUp()
+    {
+        $this->formType = new ChoiceAttributeFormType();
 
+        parent::setUp();
+    }
+
+    /**
+     * Tests the form submit
+     */
+    public function testSubmitValidData()
+    {
+        $form = $this->factory->create($this->formType);
+
+        $choice1 = new ChoiceAttributeChoice();
+        $choice1->setName('choice 1');
+
+        $choice2 = new ChoiceAttributeChoice();
+        $choice2->setName('choice 2');
+
+        $choice = new ChoiceAttribute();
+        $choice->setAllowMultiple(true)
+               ->setChoices(new ArrayCollection(array($choice1, $choice2)))
+               ->setExpanded(false)
+               ->setName('name')
+               ->setAllowBlank(false)
+               ->setDefaultValue('default');
+
+        $form->setData($choice);
+
+        $this->assertTrue($form->isSynchronized());
+        $this->assertEquals($choice, $form->getData());
+
+        $expectedViewComponents = array('type', 'allow_multiple', 'choices', 'expanded', 'name', 'allow_blank', 'default_value');
+        $view = $form->createView();
+        foreach ($expectedViewComponents as $component) {
+            $this->assertArrayHasKey($component, $view->children);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return array
+     */
+    protected function getExtensions()
+    {
+        $extensions = parent::getExtensions();
+        $extensions[] = new CoreExtension();
+
+        $subType = new ChoiceAttributeChoiceFormType();
+
+        $extensions[] = new PreloadedExtension(
+            array($subType->getName() => $subType),
+            array()
+        );
+
+        return $extensions;
+    }
 }
