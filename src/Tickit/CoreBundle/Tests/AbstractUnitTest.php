9834f5d9adccfe5efdbdcc9dd6dffcd121fc22ee
James Halsall <james.t.halsall@googlemail.com>
2013-09-28 15:35:40 +0100

    Adding AbstractUnitTest to help with mocking some symfony components

---
+++ src/Tickit/CoreBundle/Tests/AbstractUnitTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tickit\CoreBundle\Tests;
+
+/**
+ * Class AbstractUnitTest
+ *
+ * @package Tickit\CoreBundle\Tests
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class AbstractUnitTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Returns mock EntityManager
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getMockEntityManager()
+    {
+        return $this->getMockBuilder('\Doctrine\ORM\EntityManager')
+                    ->disableOriginalConstructor()
+                    ->getMock();
+    }
+
+    /**
+     * Returns mock SecurityContext
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getMockSecurityContext()
+    {
+        return $this->getMockBuilder('\Symfony\Component\Security\Core\SecurityContext')
+                    ->disableOriginalConstructor()
+                    ->getMock();
+    }
+
+    /**
+     * Returns mock UsernamePasswordToken
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getMockUsernamePasswordToken()
+    {
+        return $this->getMockBuilder('\Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken')
+                    ->disableOriginalConstructor()
+                    ->getMock();
+    }
+}
