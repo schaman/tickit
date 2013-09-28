7dcc4983a2bc58762412d1a4c6b308bf5f1f873e
James Halsall <james.t.halsall@googlemail.com>
2013-09-28 23:05:21 +0100

    Adding LiteralAttributeTest

---
+++ src/Tickit/ProjectBundle/Tests/Entity/LiteralAttributeTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tickit\ProjectBundle\Tests\Entity;
+
+use Tickit\ProjectBundle\Entity\LiteralAttribute;
+
+/**
+ * LiteralAttribute tests
+ *
+ * @package Tickit\ProjectBundle\Tests\Entity
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class LiteralAttributeTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * The attribute under test
+     *
+     * @var LiteralAttribute
+     */
+    private $attribute;
+
+    /**
+     * Setup
+     */
+    protected function setUp()
+    {
+        $this->attribute = new LiteralAttribute();
+    }
+    
+    /**
+     * Tests the setValidationType() method
+     *
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSetValidationTypeThrowsExceptionForInvalidType()
+    {
+        $this->attribute->setValidationType('invalid type');
+    }
+
+    /**
+     * Tests the setValidationType() method
+     */
+    public function testSetValidationTypeAcceptsValidType()
+    {
+        $this->attribute->setValidationType(LiteralAttribute::VALIDATION_DATE);
+
+        $this->assertEquals(LiteralAttribute::VALIDATION_DATE, $this->attribute->getValidationType());
+    }
+}
