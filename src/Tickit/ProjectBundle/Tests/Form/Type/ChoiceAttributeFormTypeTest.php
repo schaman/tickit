e251c66b33cf1db35b7b5f6392dd85ff92e05bf2
James Halsall <james.t.halsall@googlemail.com>
2013-09-28 10:27:43 +0100

    Fixing a couple of bugs with bad configuration in ChoiceAttributeFormType

---
+++ src/Tickit/ProjectBundle/Tests/Form/Type/ChoiceAttributeFormTypeTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: jameshalsall
+ * Date: 28/09/2013
+ * Time: 09:57
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Tickit\ProjectBundle\Tests\Form\Type;
+
+
+class ChoiceAttributeFormTypeTest extends \PHPUnit_Framework_TestCase {
+
+}
