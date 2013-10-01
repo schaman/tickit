4c875a64742d9271ba09a52c540e5fdf4e767bc2
Mark Wilson <mwilson@rippleffect.com>
2013-10-01 08:49:17 +0100

    Add user entity display name decorator test

---
+++ src/Tickit/UserBundle/Tests/Decorator/UserEntityDisplayNameDecoratorTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tickit\UserBundle\Tests\Decorator;
+use Tickit\UserBundle\Decorator\UserEntityDisplayNameDecorator;
+use Tickit\UserBundle\Entity\User;
+
+/**
+ * User entity display name decorator tests
+ *
+ * @package Tickit\UserBundle\Tests\Decorator
+ * @author  Mark Wilson <mark@89allport.co.uk>
+ */
+class UserEntityDisplayNameDecoratorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDecoratorUserOutput()
+    {
+        $user = new User();
+        $user->setForename('Joe')->setSurname('Bloggs');
+
+        $decorator = new UserEntityDisplayNameDecorator();
+
+        $this->assertEquals('Joe Bloggs', $decorator->decorate($user));
+    }
+}
+ 
