99a328efe6b86a979812b768499877811911c866
James Halsall <james.t.halsall@googlemail.com>
2013-10-02 13:56:53 +0100

    Adding new base event objects for entity related events

---
+++ src/Tickit/CoreBundle/Event/EntityEvent.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tickit\CoreBundle\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Entity related event.
+ *
+ * @package Tickit\CoreBundle\Event
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class EntityEvent extends Event
+{
+    /**
+     * The entity that is to be deleted
+     *
+     * @var mixed
+     */
+    private $entity;
+
+    /**
+     * Constructor.
+     *
+     * @param mixed $entity The entity related to this event
+     */
+    public function __construct($entity)
+    {
+        $this->entity = $entity;
+    }
+
+    /**
+     * Gets the entity on this event
+     *
+     * @return mixed
+     */
+    public function getEntity()
+    {
+        return $this->entity;
+    }
+
+    /**
+     * Sets the entity on this event
+     *
+     * @param mixed $entity The entity to set
+     *
+     * @return mixed
+     */
+    public function setEntity($entity)
+    {
+        $this->entity = $entity;
+    }
+}
