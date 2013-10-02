39c743ba3b70b9425c1fe29d40e61095c1991861
James Halsall <james.t.halsall@googlemail.com>
2013-10-02 14:12:56 +0100

    Adding EntityEvent tests

---
+++ src/Tickit/CoreBundle/Tests/Event/EntityEventTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tickit\UserBundle\Tests\Event;
+
+use MyProject\Proxies\__CG__\OtherProject\Proxies\__CG__\stdClass;
+use Tickit\UserBundle\Entity\User;
+use Tickit\CoreBundle\Event\EntityEvent;
+
+/**
+ * EntityEvent tests
+ *
+ * @package Tickit\CoreBundle\Tests\Event
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class EntityEventTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Dummy entity object
+     *
+     * @var stdClass
+     */
+    private $entity;
+
+    /**
+     * Event under test
+     *
+     * @var EntityEvent
+     */
+    private $event;
+
+    /**
+     * Setup
+     */
+    protected function setUp()
+    {
+        $this->entity = new \stdClass();
+        $this->event = new EntityEvent($this->entity);
+    }
+
+    /**
+     * Tests the __construct() method
+     */
+    public function testConstructSetsUpEventCorrectly()
+    {
+        $this->assertSame($this->entity, $this->event->getEntity());
+    }
+
+    /**
+     * Tests the setUser() method
+     */
+    public function testSetEntitySetsCorrectValue()
+    {
+        $entity = new \stdClass();
+        $this->event->setEntity($entity);
+
+        $this->assertSame($entity, $this->event->getEntity());
+    }
+}
