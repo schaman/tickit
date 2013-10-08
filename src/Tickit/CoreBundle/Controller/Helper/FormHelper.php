78cd81814654193f037e2c789e3350d328e0b4a3
James Halsall <james.t.halsall@googlemail.com>
2013-10-08 19:30:00 +0100

    Adding FormFactory as a dependency for FormHelper

--- src/Tickit/CoreBundle/Controller/Helper/FormHelper.php
+++ src/Tickit/CoreBundle/Controller/Helper/FormHelper.php
@@ -3,6 +3,8 @@
 namespace Tickit\CoreBundle\Controller\Helper;
 
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\FormInterface;
 
 /**
@@ -23,13 +25,22 @@ class FormHelper
     protected $templateEngine;
 
     /**
+     * A form factory
+     *
+     * @var FormFactoryInterface
+     */
+    protected $formFactory;
+
+    /**
      * Constructor.
      *
-     * @param EngineInterface $templateEngine The templating engine
+     * @param EngineInterface      $templateEngine The templating engine
+     * @param FormFactoryInterface $formFactory    A form factory
      */
-    public function __construct(EngineInterface $templateEngine)
+    public function __construct(EngineInterface $templateEngine, FormFactoryInterface $formFactory)
     {
         $this->templateEngine = $templateEngine;
+        $this->formFactory = $formFactory;
     }
 
     /**
@@ -47,5 +58,19 @@ class FormHelper
             $template,
             array_merge(array('form' => $form->createView()), $additionalParams)
         );
+    }
+
+    /**
+     * Creates a form from a type
+     *
+     * @param mixed $type    The form type to create
+     * @param mixed $data    The data to instantiate the form with
+     * @param array $options An optional array of form options
+     *
+     * @return Form
+     */
+    public function createForm($type, $data, array $options = null)
+    {
+        return $this->formFactory->create($type, $data, $options);
     }
 }
