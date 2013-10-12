df5213e2e64b435307a097df0ff8d055ec14f2d6
James Halsall <james.t.halsall@googlemail.com>
2013-10-12 15:34:33 +0100

    Fixing a couple of minor bugs in FormHelper found during testing

--- src/Tickit/CoreBundle/Controller/Helper/FormHelper.php
+++ src/Tickit/CoreBundle/Controller/Helper/FormHelper.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\Form\Form;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Form controller helper.
@@ -54,10 +55,12 @@ class FormHelper
      */
     public function renderForm($template, FormInterface $form, array $additionalParams = array())
     {
-        return $this->templateEngine->render(
+        $content = $this->templateEngine->render(
             $template,
             array_merge(array('form' => $form->createView()), $additionalParams)
         );
+
+        return new Response($content);
     }
 
     /**
@@ -69,7 +72,7 @@ class FormHelper
      *
      * @return Form
      */
-    public function createForm($type, $data, array $options = null)
+    public function createForm($type, $data, array $options = array())
     {
         return $this->formFactory->create($type, $data, $options);
     }
