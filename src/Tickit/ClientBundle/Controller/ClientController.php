96a8298cdea39f3ee7fe7c6c80b5039c23b4a70b
James Halsall <james.t.halsall@googlemail.com>
2013-10-19 13:14:41 +0100

    Adding ClientController:createAction() and tests

--- src/Tickit/ClientBundle/Controller/ClientController.php
+++ src/Tickit/ClientBundle/Controller/ClientController.php
@@ -2,6 +2,13 @@
 
 namespace Tickit\ClientBundle\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Tickit\ClientBundle\Entity\Client;
+use Tickit\ClientBundle\Manager\ClientManager;
+use Tickit\CoreBundle\Controller\Helper\BaseHelper;
+use Tickit\CoreBundle\Controller\Helper\CsrfHelper;
+use Tickit\CoreBundle\Controller\Helper\FormHelper;
+
 /**
  * Client controller.
  *
@@ -10,7 +17,6 @@ namespace Tickit\ClientBundle\Controller;
  * @package Tickit\ClientBundle\Controller
  * @author  James Halsall <james.t.halsall@googlemail.com>
  */
- 
 class ClientController
 {
     /**
@@ -19,4 +25,76 @@ class ClientController
      * @const string
      */
     const CSRF_DELETE_INTENTION = 'delete_client';
+
+    /**
+     * The form controller helper
+     *
+     * @var FormHelper
+     */
+    protected $formHelper;
+
+    /**
+     * The base controller helper
+     *
+     * @var BaseHelper
+     */
+    protected $baseHelper;
+
+    /**
+     * The CSRF controller helper
+     *
+     * @var CsrfHelper
+     */
+    protected $csrfHelper;
+
+    /**
+     * The client manager
+     *
+     * @var ClientManager
+     */
+    protected $clientManager;
+
+    /**
+     * Constructor.
+     *
+     * @param FormHelper    $formHelper    The form controller helper
+     * @param BaseHelper    $baseHelper    The base controller helper
+     * @param CsrfHelper    $csrfHelper    The CSRF controller helper
+     * @param ClientManager $clientManager The client manager
+     */
+    public function __construct(
+        FormHelper $formHelper,
+        BaseHelper $baseHelper,
+        CsrfHelper $csrfHelper,
+        ClientManager $clientManager
+    ) {
+        $this->formHelper = $formHelper;
+        $this->baseHelper = $baseHelper;
+        $this->csrfHelper = $csrfHelper;
+        $this->clientManager = $clientManager;
+    }
+
+    /**
+     * Create action.
+     *
+     * Handles a form submission and creates a new Client entity with the form data
+     *
+     * @return JsonResponse
+     */
+    public function createAction()
+    {
+        $responseData = ['success' => false];
+        $form = $this->formHelper->createForm('tickit_client', new Client());
+        $form->handleRequest($this->baseHelper->getRequest());
+
+        if ($form->isValid()) {
+            $this->clientManager->create($form->getData());
+            $responseData['success'] = true;
+        } else {
+            $formResponse = $this->formHelper->renderForm('TickitClientBundle:Client:create.html.twig', $form);
+            $responseData['form'] = $formResponse->getContent();
+        }
+
+        return new JsonResponse($responseData);
+    }
 }
---
+++ src/Tickit/ClientBundle/Tests/Controller/ClientControllerTest.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace Tickit\ClientBundle\Tests\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Tickit\ClientBundle\Controller\ClientController;
+use Tickit\ClientBundle\Entity\Client;
+use Tickit\CoreBundle\Tests\AbstractUnitTest;
+
+/**
+ * ClientController tests
+ *
+ * @package Tickit\ClientBundle\Tests\Controller
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class ClientControllerTest extends AbstractUnitTest
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $formHelper;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $baseHelper;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $csrfHelper;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $clientManager;
+
+    /**
+     * Setup
+     */
+    protected function setUp()
+    {
+        $this->formHelper = $this->getMockFormHelper();
+        $this->baseHelper = $this->getMockBaseHelper();
+        $this->csrfHelper = $this->getMockCsrfHelper();
+
+        $this->clientManager = $this->getMockBuilder('\Tickit\ClientBundle\Manager\ClientManager')
+                                    ->disableOriginalConstructor()
+                                    ->getMock();
+    }
+    
+    /**
+     * Tests the createAction() method
+     */
+    public function testCreateActionCreatesClientForValidForm()
+    {
+        $client = new Client();
+
+        $form = $this->getMockForm();
+        $this->trainFormHelperToCreateForm('tickit_client', $client, $form);
+
+        $request = new Request();
+        $this->trainBaseHelperToReturnRequest($request);
+        $this->trainFormToHandleRequest($form, $request);
+        $this->trainFormToBeValid($form);
+        $this->trainFormToReturnClient($form, $client);
+        $this->clientManager->expects($this->once())
+                            ->method('create')
+                            ->with($client);
+
+        $expectedData = ['success' => true];
+        $response = $this->getController()->createAction();
+        $this->assertEquals($expectedData, json_decode($response->getContent(), true));
+    }
+
+    /**
+     * Tests the createAction() method
+     */
+    public function testCreateActionHandlesInvalidForm()
+    {
+        $client = new Client();
+
+        $form = $this->getMockForm();
+        $this->trainFormHelperToCreateForm('tickit_client', $client, $form);
+
+        $request = new Request();
+        $this->trainBaseHelperToReturnRequest($request);
+        $this->trainFormToHandleRequest($form, $request);
+        $this->trainFormToBeInvalid($form);
+        $this->clientManager->expects($this->never())
+                            ->method('create');
+
+        $this->formHelper->expects($this->once())
+                         ->method('renderForm')
+                         ->with('TickitClientBundle:Client:create.html.twig', $form)
+                         ->will($this->returnValue(new Response('form-content')));
+
+        $expectedData = ['success' => false, 'form' => 'form-content'];
+        $response = $this->getController()->createAction();
+
+        $this->assertEquals($expectedData, json_decode($response->getContent(), true));
+    }
+
+    /**
+     * Gets a new controller instance
+     *
+     * @return ClientController
+     */
+    private function getController()
+    {
+        return new ClientController($this->formHelper, $this->baseHelper, $this->csrfHelper, $this->clientManager);
+    }
+
+    private function trainBaseHelperToReturnRequest(Request $request)
+    {
+        $this->baseHelper->expects($this->once())
+                         ->method('getRequest')
+                         ->will($this->returnValue($request));
+    }
+
+    private function trainFormToHandleRequest(\PHPUnit_Framework_MockObject_MockObject $form, Request $request)
+    {
+        $form->expects($this->once())
+             ->method('handleRequest')
+             ->with($request);
+    }
+
+    private function trainFormToBeValid(\PHPUnit_Framework_MockObject_MockObject $form)
+    {
+        $form->expects($this->once())
+             ->method('isValid')
+             ->will($this->returnValue(true));
+    }
+
+    private function trainFormToBeInvalid(\PHPUnit_Framework_MockObject_MockObject $form)
+    {
+        $form->expects($this->once())
+             ->method('isValid')
+             ->will($this->returnValue(false));
+    }
+
+    private function trainFormToReturnClient(\PHPUnit_Framework_MockObject_MockObject $form, Client $client)
+    {
+        $form->expects($this->once())
+             ->method('getData')
+             ->will($this->returnValue($client));
+
+    }
+
+    private function trainFormHelperToCreateForm($type, $data, $returnedForm)
+    {
+        $this->formHelper->expects($this->once())
+                         ->method('createForm')
+                         ->with($type, $data)
+                         ->will($this->returnValue($returnedForm));
+    }
+}
