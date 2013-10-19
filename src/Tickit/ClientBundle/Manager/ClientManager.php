3dcfee46e2065c497b1f89afb764124ed620f3e8
James Halsall <james.t.halsall@googlemail.com>
2013-10-19 12:21:24 +0100

    Adding ClientManager and tests

---
+++ src/Tickit/ClientBundle/Manager/ClientManager.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tickit\ClientBundle\Manager;
+
+use Doctrine\Common\Persistence\ObjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Tickit\ClientBundle\Entity\Repository\ClientRepository;
+use Tickit\CoreBundle\Event\Dispatcher\AbstractEntityEventDispatcher;
+use Tickit\CoreBundle\Manager\AbstractManager;
+
+/**
+ * Client Manager.
+ *
+ * Responsible for managing Client entities in the application.
+ *
+ * @package Tickit\ClientBundle\Manager
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class ClientManager extends AbstractManager
+{
+    /**
+     * The client repository
+     *
+     * @var ClientRepository
+     */
+    private $clientRepository;
+
+    /**
+     * Constructor.
+     *
+     * @param ClientRepository              $clientRepository The client repository
+     * @param EntityManagerInterface        $em               An entity manager
+     * @param AbstractEntityEventDispatcher $eventDispatcher  An event dispatcher
+     */
+    public function __construct(
+        ClientRepository $clientRepository,
+        EntityManagerInterface $em,
+        AbstractEntityEventDispatcher $eventDispatcher
+    ) {
+        $this->clientRepository = $clientRepository;
+
+        parent::__construct($em, $eventDispatcher);
+    }
+
+    /**
+     * Gets the entity repository.
+     *
+     * This method returns the entity repository that is associated with this manager's entity.
+     *
+     * @return ClientRepository
+     */
+    public function getRepository()
+    {
+        return $this->clientRepository;
+    }
+
+    /**
+     * Returns the original Client from the entity manager.
+     *
+     * This method takes an entity and returns a copy in its original state
+     * from the entity manager. This is used when dispatching entity update
+     * events, so a before and after comparison can take place.
+     *
+     * @param object $entity The entity in its current state
+     *
+     * @return object
+     */
+    protected function fetchEntityInOriginalState($entity)
+    {
+        return $this->clientRepository->find($entity->getId());
+    }
+}
---
+++ src/Tickit/ClientBundle/Tests/Manager/ClientManagerTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tickit\ClientBundle\Tests\Manager;
+
+use Tickit\ClientBundle\Manager\ClientManager;
+use Tickit\CoreBundle\Tests\AbstractUnitTest;
+
+/**
+ * ClientManager tests
+ *
+ * @package Tickit\ClientBundle\Tests\Manager
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class ClientManagerTest extends AbstractUnitTest
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $clientRepo;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $em;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $eventDispatcher;
+
+    /**
+     * Setup
+     */
+    protected function setUp()
+    {
+        $this->clientRepo = $this->getMockBuilder('\Tickit\ClientBundle\Entity\Repository\ClientRepository')
+                                 ->disableOriginalConstructor()
+                                 ->getMock();
+
+        $this->em = $this->getMockEntityManager();
+        $this->eventDispatcher = $this->getMockEventDispatcher();
+    }
+    
+    /**
+     * Tests the getRepository() method
+     */
+    public function testGetRepositoryReturnsCorrectInstance()
+    {
+        $this->assertSame($this->clientRepo, $this->getManager()->getRepository());
+    }
+
+    /**
+     * Gets a new manager instance
+     *
+     * @return ClientManager
+     */
+    private function getManager()
+    {
+        return new ClientManager($this->clientRepo, $this->em, $this->eventDispatcher);
+    }
+}
