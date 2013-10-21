861f0258ae20385fe6b0fe4eb1105fa4e864e293
James Halsall <james.t.halsall@googlemail.com>
2013-10-21 20:08:40 +0100

    Adding Client model

---
+++ src/Tickit/ClientBundle/Resources/public/js/models/Client.js
@@ -0,0 +1,34 @@
+/**
+ * Client model
+ *
+ * @type {Backbone.Model}
+ */
+define(['backbone'], function(Backbone) {
+    return Backbone.Model.extend({
+
+        defaults: {
+            id: null,
+            name: '',
+            url: '',
+            notes: ''
+        },
+
+        /**
+         * Gets the edit URL for this client
+         *
+         * @return {string}
+         */
+        getEditUrl : function() {
+            return Routing.generate('client_edit_view', { id: this.get('id') });
+        },
+
+        /**
+         * Gets the delete URL for this client
+         *
+         * @return {string}
+         */
+        getDeleteUrl : function() {
+            return Routing.generate('client_delete', { id: this.get('id'), token: this.get('csrf_token') });
+        }
+    });
+});
