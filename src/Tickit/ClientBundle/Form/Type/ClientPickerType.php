e5ff83a01eb647f07846182da57d7c25308ee50e
James Halsall <james.t.halsall@googlemail.com>
2013-10-24 12:36:18 +0100

    Adding ClientPickerType and associated service configuration

---
+++ src/Tickit/ClientBundle/Form/Type/ClientPickerType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tickit\ClientBundle\Form\Type;
+
+use Tickit\CoreBundle\Form\Type\Picker\AbstractPickerType;
+
+/**
+ * Client picker field type.
+ *
+ * Provides a field for picking a single client.
+ *
+ * @package Tickit\ClientBundle\Form\Type
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class ClientPickerType extends AbstractPickerType
+{
+    /**
+     * Gets the name of the field that stores the picker IDs
+     *
+     * @return mixed
+     */
+    public function getFieldName()
+    {
+        return 'client';
+    }
+
+    /**
+     * Gets the restriction type (if any)
+     *
+     * @return string|null
+     */
+    public function getRestriction()
+    {
+        return static::RESTRICTION_SINGLE;
+    }
+
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return 'tickit_client_picker';
+    }
+}
+ 
