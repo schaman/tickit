6ab1a28cbed4c65b00eaf69874d8b1dda697598d
James Halsall <james.t.halsall@googlemail.com>
2013-10-24 12:24:12 +0100

    Adding ClientEntityNameDecorator for decorating client entities into string representations

---
+++ src/Tickit/ClientBundle/Decorator/ClientEntityNameDecorator.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tickit\ClientBundle\Decorator;
+
+use Tickit\ClientBundle\Entity\Client;
+use Tickit\ClientBundle\Manager\ClientManager;
+
+/**
+ * Client entity name decorator.
+ *
+ * Decorates a client entity with a name.
+ *
+ * @package Tickit\ClientBundle\Decorator
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class ClientEntityNameDecorator
+{
+    /**
+     * Decorates a client entity
+     *
+     * @param Client $client The client ID to decorate
+     *
+     * @return string
+     */
+    public function decorate(Client $client)
+    {
+        return $client->getName();
+    }
+}
+ 
---
+++ src/Tickit/ClientBundle/Tests/Decorator/ClientEntityNameDecoratorTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tickit\ClientBundle\Tests\Decorator;
+
+use Tickit\ClientBundle\Decorator\ClientEntityNameDecorator;
+use Tickit\ClientBundle\Entity\Client;
+
+/**
+ * ClientEntityNameDecorator tests
+ *
+ * @package Tickit\ClientBundle\Tests\Decorator
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class ClientEntityNameDecoratorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Tests the decorate() method
+     */
+    public function testDecorateCorrectlyDecoratesClient()
+    {
+        $client = new Client();
+        $client->setName('expected name');
+
+        $this->assertEquals($client->getName(), $this->getDecorator()->decorate($client));
+    }
+
+    /**
+     * Gets a new decorator instance
+     *
+     * @return ClientEntityNameDecorator
+     */
+    private function getDecorator()
+    {
+        return new ClientEntityNameDecorator();
+    }
+}
+ 
