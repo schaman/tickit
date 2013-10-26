c645fed4a27eb671432079508556d8863e6a3df0
James Halsall <james.t.halsall@googlemail.com>
2013-10-26 10:23:14 +0100

    Updating AbstractPickerDataTransformer so it is restriction-aware and returns the correct return type

--- src/Tickit/CoreBundle/Form/Type/Picker/DataTransformer/AbstractPickerDataTransformer.php
+++ src/Tickit/CoreBundle/Form/Type/Picker/DataTransformer/AbstractPickerDataTransformer.php
@@ -54,8 +54,6 @@ abstract class AbstractPickerDataTransformer implements DataTransformerInterface
      * @param mixed $value The value in the original representation
      *
      * @return mixed The value in the transformed representation
-     *
-     * @throws TransformationFailedException When the transformation fails.
      */
     public function transform($value)
     {
@@ -63,32 +61,11 @@ abstract class AbstractPickerDataTransformer implements DataTransformerInterface
             return '';
         }
 
-        if (!$value instanceof Collection) {
-            if (false === is_array($value)) {
-                $value = [$value];
-            }
-            $value = new ArrayCollection($value);
+        if ($this->restriction === AbstractPickerType::RESTRICTION_SINGLE) {
+            return $this->transformEntity($value);
+        } else {
+            return $this->transformCollection($value);
         }
-
-        $identifier = $this->getEntityIdentifier();
-        $method = "get" . ucfirst($identifier);
-        $flattened = array_map(
-            function ($entity) use ($method) {
-                if (!method_exists($entity, $method)) {
-                    throw new \RuntimeException(
-                        sprintf(
-                            'The method %s() does not exist and/or is not public on %s',
-                            $method,
-                            get_class($entity)
-                        )
-                    );
-                }
-                return $entity->{$method}();
-            },
-            $value->toArray()
-        );
-
-        return implode(',', $flattened);
     }
 
     /**
@@ -113,8 +90,91 @@ abstract class AbstractPickerDataTransformer implements DataTransformerInterface
             return null;
         }
 
+        if ($this->restriction === AbstractPickerType::RESTRICTION_SINGLE) {
+            return $this->reverseTransformSingleIdentifier($value);
+        } else {
+            return $this->reverseTransformMultipleIdentifiers($value);
+        }
+    }
+
+    /**
+     * Transforms a single entity instance
+     *
+     * @param mixed $entity The entity to transform
+     *
+     * @throws TransformationFailedException If the provided $entity is actually a Collection instance
+     *
+     * @return mixed
+     */
+    private function transformEntity($entity)
+    {
+        if ($entity instanceof Collection) {
+            throw new TransformationFailedException();
+        }
+
+        $method = $this->getIdentifierAccessorName();
+        $this->validateMethodExists($entity, $method);
+
+        return $entity->{$method}();
+    }
+
+    /**
+     * Transforms a collection of entity instances
+     *
+     * @param Collection $collection The collection to transform
+     *
+     * @throws TransformationFailedException If the $collection argument is not actually a collection
+     *
+     * @return string
+     */
+    private function transformCollection($collection)
+    {
+        if (!$collection instanceof Collection) {
+            throw new TransformationFailedException();
+        }
+
+        $flattened = array_map(
+            function ($entity) {
+                return $this->transformEntity($entity);
+            },
+            $collection->toArray()
+        );
+
+        return implode(',', $flattened);
+    }
+
+    /**
+     * Reverse transforms a single identifier into an entity
+     *
+     * @param string|integer $identifier The identifier
+     *
+     * @throws TransformationFailedException If the $identifier is a string of more than one identifier
+     *
+     * @return mixed
+     */
+    private function reverseTransformSingleIdentifier($identifier)
+    {
+        $identifier = explode(',', $identifier);
+        if (count($identifier) > 1) {
+            throw new TransformationFailedException(
+                'More than one identifier was provided to reverseTransform() with single select restriction enabled'
+            );
+        }
+
+        return $this->findEntityByIdentifier($identifier[0]);
+    }
+
+    /**
+     * Reverse transforms a string of comma separated identifiers into a collection of entities
+     *
+     * @param string $identifiers The string of identifiers
+     *
+     * @return ArrayCollection
+     */
+    private function reverseTransformMultipleIdentifiers($identifiers)
+    {
         $entities = [];
-        $values = explode(',', $value);
+        $values = explode(',', $identifiers);
         foreach ($values as $identifier) {
             $entity = $this->findEntityByIdentifier($identifier);
             if (null === $entity) {
@@ -124,6 +184,36 @@ abstract class AbstractPickerDataTransformer implements DataTransformerInterface
         }
 
         return new ArrayCollection($entities);
+    }
+
+    /**
+     * @return string
+     */
+    private function getIdentifierAccessorName()
+    {
+        $identifier = $this->getEntityIdentifier();
+        return "get" . ucfirst($identifier);
+    }
+
+    /**
+     * Validates that a method exists on an object.
+     *
+     * @param mixed  $object The object to check
+     * @param string $method The method name to check
+     *
+     * @throws \RuntimeException If the method does not exist
+     */
+    private function validateMethodExists($object, $method)
+    {
+        if (!method_exists($object, $method)) {
+            throw new \RuntimeException(
+                sprintf(
+                    'The method %s() does not exist and/or is not public on %s',
+                    $method,
+                    get_class($object)
+                )
+            );
+        }
     }
 
     /**
--- src/Tickit/CoreBundle/Tests/Form/Type/Picker/DataTransformer/AbstractPickerDataTransformerTest.php
+++ src/Tickit/CoreBundle/Tests/Form/Type/Picker/DataTransformer/AbstractPickerDataTransformerTest.php
@@ -3,6 +3,7 @@
 namespace Tickit\CoreBundle\Tests\Form\Type\Picker\DataTransformer;
 
 use Doctrine\Common\Collections\ArrayCollection;
+use Tickit\CoreBundle\Form\Type\Picker\AbstractPickerType;
 use Tickit\CoreBundle\Form\Type\Picker\DataTransformer\AbstractPickerDataTransformer;
 use Tickit\CoreBundle\Tests\Form\Type\Picker\Mock\MockEntity;
 
@@ -58,7 +59,21 @@ class AbstractPickerDataTransformerTest extends \PHPUnit_Framework_TestCase
                   ->method('getEntityIdentifier')
                   ->will($this->returnValue('invalid'));
 
+        $this->sut->setRestriction(AbstractPickerType::RESTRICTION_SINGLE);
+
         $this->sut->transform(new MockEntity(1));
+    }
+
+    /**
+     * Tests the transform() method
+     */
+    public function testTransformHandlesSingleEntityWithSingleSelectRestrictionEnabled()
+    {
+        $this->trainTransformerToReturnIdentifier();
+        $this->sut->setRestriction(AbstractPickerType::RESTRICTION_SINGLE);
+
+        $expectedData = '1';
+        $this->assertEquals($expectedData, $this->sut->transform(new MockEntity(1)));
     }
 
     /**
@@ -78,10 +93,26 @@ class AbstractPickerDataTransformerTest extends \PHPUnit_Framework_TestCase
 
     /**
      * Tests the transform() method
+     *
+     * @expectedException \Symfony\Component\Form\Exception\TransformationFailedException
+     */
+    public function testTransformThrowsExceptionForCollectionWithSingleRestrictionEnabled()
+    {
+        $this->sut->setRestriction(AbstractPickerType::RESTRICTION_SINGLE);
+
+        $collection = new ArrayCollection(
+            [new MockEntity(1), new MockEntity(2), new MockEntity(4)]
+        );
+
+        $this->sut->transform($collection);
+    }
+
+    /**
+     * Tests the transform() method
      */
     public function testTransformHandlesEntityCollectionCorrectly()
     {
-        $this->trainTransformerToReturnIdentifier();
+        $this->trainTransformerToReturnIdentifier(3);
 
         $collection = new ArrayCollection(
             [new MockEntity(1), new MockEntity(2), new MockEntity(4)]
@@ -137,6 +168,21 @@ class AbstractPickerDataTransformerTest extends \PHPUnit_Framework_TestCase
     /**
      * Tests the reverseTransform() method
      */
+    public function testReverseTransformHandlesSingleIdentifierWithSingleRestrictionEnabledCorrectly()
+    {
+        $this->sut->setRestriction(AbstractPickerType::RESTRICTION_SINGLE);
+        $this->sut->expects($this->once())
+                  ->method('findEntityByIdentifier')
+                  ->with(1)
+                  ->will($this->returnValue(new MockEntity(1)));
+
+        $expected = new MockEntity(1);
+        $this->assertEquals($expected, $this->sut->reverseTransform('1'));
+    }
+
+    /**
+     * Tests the reverseTransform() method
+     */
     public function testReverseTransformHandlesMultipleIdentifiersCorrectly()
     {
         $this->sut->expects($this->exactly(2))
@@ -155,9 +201,21 @@ class AbstractPickerDataTransformerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($expected, $this->sut->reverseTransform('1, 2'));
     }
 
-    private function trainTransformerToReturnIdentifier()
+    /**
+     * Tests the reverseTransform() method
+     *
+     * @expectedException \Symfony\Component\Form\Exception\TransformationFailedException
+     */
+    public function testReverseTransformThrowsExceptionForMultipleIdentifiersWithSingleRestrictionEnabled()
     {
-        $this->sut->expects($this->once())
+        $this->sut->setRestriction(AbstractPickerType::RESTRICTION_SINGLE);
+
+        $this->sut->reverseTransform('1,3,4,5');
+    }
+
+    private function trainTransformerToReturnIdentifier($times = 1)
+    {
+        $this->sut->expects($this->exactly($times))
                   ->method('getEntityIdentifier')
                   ->will($this->returnValue('id'));
     }
