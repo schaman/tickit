a37e86354b484d008310802793ce4f11b3ec8b71
James Halsall <james.t.halsall@googlemail.com>
2013-11-09 20:44:07 +0000

    Updating ClientEventDispatcher to be more compatible with abstract dispatcher, fixing some parameters in service config

--- src/Tickit/Bundle/ClientBundle/Event/Dispatcher/ClientEventDispatcher.php
+++ src/Tickit/Bundle/ClientBundle/Event/Dispatcher/ClientEventDispatcher.php
@@ -22,10 +22,7 @@
 namespace Tickit\Bundle\ClientBundle\Event\Dispatcher;
 
 use Tickit\Bundle\ClientBundle\TickitClientEvents;
-use Tickit\Bundle\CoreBundle\Event\AbstractVetoableEvent;
-use Tickit\Bundle\CoreBundle\Event\Dispatcher\AbstractEntityEventDispatcher;
-use Tickit\Bundle\CoreBundle\Event\EntityEvent;
-use Tickit\Bundle\CoreBundle\Event\EntityModifiedEvent;
+use Tickit\Component\Event\Dispatcher\AbstractEntityEventDispatcher;
 
 /**
  * Client event dispatcher.
@@ -36,90 +33,19 @@ use Tickit\Bundle\CoreBundle\Event\EntityModifiedEvent;
 class ClientEventDispatcher extends AbstractEntityEventDispatcher
 {
     /**
-     * Dispatches events for the "before create" event on the entity
+     * Gets an array of event names
      *
-     * @param object $entity The project entity that is about to be created
-     *
-     * @return AbstractVetoableEvent
+     * @return array
      */
-    public function dispatchBeforeCreateEvent($entity)
+    protected function getEventNames()
     {
-        $beforeEvent = new EntityEvent($entity);
-        $beforeEvent = $this->dispatcher->dispatch(TickitClientEvents::CLIENT_BEFORE_CREATE, $beforeEvent);
-
-        return $beforeEvent;
-    }
-
-    /**
-     * Dispatches events for the "create" event on the entity
-     *
-     * @param object $entity The project entity that has just been created
-     *
-     * @return void
-     */
-    public function dispatchCreateEvent($entity)
-    {
-        $event = new EntityEvent($entity);
-        $this->dispatcher->dispatch(TickitClientEvents::CLIENT_CREATE, $event);
-    }
-
-    /**
-     * Dispatches events for the "before update" event on the entity
-     *
-     * @param object $entity The entity that is about to be updated
-     *
-     * @return AbstractVetoableEvent
-     */
-    public function dispatchBeforeUpdateEvent($entity)
-    {
-        $beforeEvent = new EntityEvent($entity);
-        $beforeEvent = $this->dispatcher->dispatch(TickitClientEvents::CLIENT_BEFORE_UPDATE, $beforeEvent);
-
-        return $beforeEvent;
-    }
-
-    /**
-     * Dispatches events for the "update" event on the entity
-     *
-     * @param object $entity         The entity that has just been updated
-     * @param object $originalEntity The entity before any changes were applied
-     *
-     * @return void
-     */
-    public function dispatchUpdateEvent($entity, $originalEntity)
-    {
-        $event = new EntityModifiedEvent($entity, $originalEntity);
-        $this->dispatcher->dispatch(TickitClientEvents::CLIENT_UPDATE, $event);
-    }
-
-    /**
-     * Dispatches events for the "before delete" event on the entity
-     *
-     * The implementing class should return the event object to guarantee
-     * expected behaviour
-     *
-     * @param object $entity The entity that is about to be deleted
-     *
-     * @return AbstractVetoableEvent
-     */
-    public function dispatchBeforeDeleteEvent($entity)
-    {
-        $beforeEvent = new EntityEvent($entity);
-        $beforeEvent = $this->dispatcher->dispatch(TickitClientEvents::CLIENT_BEFORE_DELETE, $beforeEvent);
-
-        return $beforeEvent;
-    }
-
-    /**
-     * Dispatches events for the "delete" event on the entity
-     *
-     * @param object $entity The entity that has just been deleted in the entity manager
-     *
-     * @return void
-     */
-    public function dispatchDeleteEvent($entity)
-    {
-        $event = new EntityEvent($entity);
-        $this->dispatcher->dispatch(TickitClientEvents::CLIENT_DELETE, $event);
+        return [
+            'before_create' => TickitClientEvents::CLIENT_BEFORE_CREATE,
+            'create' => TickitClientEvents::CLIENT_CREATE,
+            'before_update' => TickitClientEvents::CLIENT_BEFORE_UPDATE,
+            'update' => TickitClientEvents::CLIENT_UPDATE,
+            'before_delete' => TickitClientEvents::CLIENT_BEFORE_DELETE,
+            'delete' => TickitClientEvents::CLIENT_DELETE
+        ];
     }
 }
