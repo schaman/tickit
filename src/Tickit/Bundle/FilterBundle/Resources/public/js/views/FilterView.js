23a0af16f220b780eee21e031e1f4e8e0e2b18b6
James Halsall <james.t.halsall@googlemail.com>
2013-11-30 11:41:58 +0000

    Adding support for formUrl, and fetching filter values in the FilterView

--- src/Tickit/Bundle/FilterBundle/Resources/public/js/views/FilterView.js
+++ src/Tickit/Bundle/FilterBundle/Resources/public/js/views/FilterView.js
@@ -19,18 +19,46 @@ define(['modules/template'], function(Template) {
         /**
          * Initialise the view
          */
-        initialize : function() {
-            if (null === this.el) {
-                throw new Error('You must provide an "el" property to reference the element containing the filters');
+        initialize : function(options) {
+            if (!options.formUrl) {
+                throw new Error('You must provide a "formUrl" property where the filter form content will be fetched from');
             }
+
+            if (null === options.el) {
+                throw new Error('You must provide an "el" property to reference the filters container');
+            }
+
+            this.fetchFilterForm(options.formUrl);
         },
 
         /**
-         * Gets the current filter values
+         * Fetches the filter form content from a source URL
+         *
+         * @param {String} url The URL where we can fetch the filters from
+         */
+        fetchFilterForm : function(url) {
+            var t = this;
+            Template.fetch(url, function(tpl) {
+                t.el.html(tpl);
+            });
+        },
+
+        /**
+         * Gets the current filter values.
+         *
+         * Will return an object where the field names are object
+         * properties, with their corresponding values.
+         *
+         * @return {object}
          */
         getFilterValues : function() {
-            // TODO: iterate over this.el and return all current filter values
-            return {};
+            var values = this.el.serializeArray();
+            var flat = {};
+            _.each(values, function(field) {
+                flat[field.name] = field.value;
+            });
+
+            return flat;
         },
 
         /**
