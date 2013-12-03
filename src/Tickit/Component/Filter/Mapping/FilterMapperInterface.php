2c057613e7f24c6abc08f2dff14338baef0d9271
James Halsall <james.t.halsall@googlemail.com>
2013-12-03 21:41:41 +0000

    Adding FilterMapperInterface

---
+++ src/Tickit/Component/Filter/Mapping/FilterMapperInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tickit\Component\Filter\Mapper;
+
+/**
+ * Filter Mapper interface.
+ *
+ * Filter mappers are responsible for returning a map of field names
+ * to the filter types that they should implement.
+ *
+ * @package Tickit\Component\Filter\Mapper
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ * @see     Tickit\Component\Filter\Collection\Builder\FilterCollectionBuilder
+ */
+interface FilterMapperInterface
+{
+    /**
+     * Gets the field map.
+     *
+     * Returns the mapping of field names to filter types.
+     *
+     * This is usually in the form of an array, where the keys are
+     * the field names and the values are the filter types
+     */
+    public function getFieldMap();
+}
+ 
