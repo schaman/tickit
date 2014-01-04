b752cf718a3337771f968f7bb24cbd786aa1ef82
James Halsall <james.t.halsall@googlemail.com>
2014-01-04 20:23:28 +0000

    Adding constants for notification events

---
+++ src/Tickit/Component/Notification/Event/NotificationEvents.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tickit\Component\Notification\Event;
+
+/**
+ * Notification event mappings.
+ *
+ * @package Tickit\Component\Notification\Event
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+final class NotificationEvents
+{
+    /**
+     * Constant representing the name  of the user notification event
+     *
+     * @const string
+     */
+    const NOTIFY_USER = 'tickit_notification.user';
+}
