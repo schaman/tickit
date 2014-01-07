2a1fc11664f2275c1e3314779d67c0689616b32c
James Halsall <james.t.halsall@googlemail.com>
2014-01-07 17:52:49 +0000

    Starting work on notification providers

---
+++ src/Tickit/Bundle/NotificationBundle/Resources/public/js/providers/PollingNotificationProvider.js
@@ -0,0 +1,18 @@
+/**
+ * Polling notification provider.
+ *
+ * Provides notification objects by polling the server at a scheduled interval
+ *
+ * @type {object}
+ */
+define(function() {
+    return {
+
+
+
+        init : function(options) {
+
+        }
+
+    };
+});
