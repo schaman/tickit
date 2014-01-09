613054829a81e387874ce83ea11e4ea2964283a1
James Halsall <james.t.halsall@googlemail.com>
2014-01-09 20:04:06 +0000

    Adding permission request logic for notifications to Html5NotificationDispatcher

--- src/Tickit/Bundle/NotificationBundle/Resources/public/js/dispatchers/Html5NotificationDispatcher.js
+++ src/Tickit/Bundle/NotificationBundle/Resources/public/js/dispatchers/Html5NotificationDispatcher.js
@@ -10,25 +10,38 @@ define([
     'notification/js/dispatchers/AbstractNotificationDispatcher'
 ], function(AbstractNotificationDispatcher) {
 
-    function Html5NotificationDispatcher() {}
-    Html5NotificationDispatcher.prototype = {
-        dispatch : function(notifications) {
-            if (!_.isArray(notifications)) {
-                notifications = [notifications];
-            }
-
-            _.each(notifications, function(n) {
-                var msg = new Notification('Tickit Notification', {
-                    dir: 'auto',
-                    tag: n.get('id'),
-                    body: n.get('message')
-                });
-                msg.onclick = function() {
-                    alert('marked as read (id: ' + n.get('id') + ')');
+    function Html5NotificationDispatcher() {
+        if (Notification.permission !== 'denied') {
+            Notification.requestPermission(function(perm) {
+                if (!("permission" in Notification)) {
+                    Notification.permission = perm;
                 }
             });
         }
-    };
+
+        return {
+            dispatch : function(notifications) {
+                if (Notification.permission !== 'granted') {
+                    return;
+                }
+
+                if (!_.isArray(notifications)) {
+                    notifications = [notifications];
+                }
+
+                _.each(notifications, function(n) {
+                    var msg = new Notification('Tickit Notification', {
+                        dir: 'auto',
+                        tag: n.get('id'),
+                        body: n.get('message')
+                    });
+                    msg.onclick = function() {
+                        alert('marked as read (id: ' + n.get('id') + ')');
+                    }
+                });
+            }
+        }
+    }
 
     _.extend(Html5NotificationDispatcher, AbstractNotificationDispatcher);
 
