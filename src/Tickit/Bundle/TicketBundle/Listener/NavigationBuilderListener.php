903dadef66388cbfc7a4aab227f0d289587ff57e
James Halsall <james.t.halsall@googlemail.com>
2014-01-25 23:10:14 +0000

    Adding NavigationBuilderListener and tests for the TicketBundle

---
+++ src/Tickit/Bundle/TicketBundle/Listener/NavigationBuilderListener.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * Tickit, an open source web based bug management tool.
+ *
+ * Copyright (C) 2013  Tickit Project <http://tickit.io>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tickit\Bundle\TicketBundle\Listener;
+
+use Tickit\Component\Navigation\Event\NavigationBuildEvent;
+use Tickit\Component\Navigation\Model\NavigationItem;
+
+/**
+ * Ticket navigation builder listener.
+ *
+ * @package Tickit\Bundle\TicketBundle\Listener
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class NavigationBuilderListener
+{
+    /**
+     * Build event for ticket navigation
+     *
+     * @param NavigationBuildEvent $event Navigation build event
+     *
+     * @return void
+     */
+    public function onBuild(NavigationBuildEvent $event)
+    {
+        switch ($event->getNavigationName()) {
+            case 'main':
+                $item = new NavigationItem(
+                    'Create Ticket',
+                    'ticket_index', // TODO: this needs updating when ticket actions are implemented
+                    15,
+                    ['icon' => 'plus', 'class' => 'add-ticket']
+                );
+                $event->addItem($item);
+                break;
+        }
+    }
+}
---
+++ src/Tickit/Bundle/TicketBundle/Tests/Listener/NavigationBuilderListenerTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tickit\Bundle\TicketBundle\Tests\Listener;
+
+use Tickit\Bundle\TicketBundle\Listener\NavigationBuilderListener;
+use Tickit\Component\Navigation\Event\NavigationBuildEvent;
+use Tickit\Component\Navigation\Model\NavigationItem;
+
+/**
+ * NavgationBuilderListener tests
+ *
+ * @package Tickit\Bundle\TicketBundle\Tests\Listener
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class NavigationBuilderListenerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Tests the onBuild() method
+     */
+    public function testOnBuildIgnoresInvalidNavigationNames()
+    {
+        $event = new NavigationBuildEvent('irrelevant name');
+        $this->getListener()->onBuild($event);
+
+        $this->assertEquals(0, $event->getItems()->count());
+    }
+
+    /**
+     * Tests the onBuild() method
+     */
+    public function testOnBuildBuildsCorrectNavigationItem()
+    {
+        $event = new NavigationBuildEvent('main');
+        $this->getListener()->onBuild($event);
+
+        $items = $event->getItems();
+        $this->assertEquals(1, $items->count());
+
+        /** @var NavigationItem $first */
+        $first = $items->current();
+        $this->assertEquals('Create Ticket', $first->getText());
+        $this->assertEquals('ticket_index', $first->getRouteName());
+        $params = $first->getParams();
+        $this->assertEquals('plus', $params['icon']);
+        $this->assertEquals('add-ticket', $params['class']);
+    }
+
+    /**
+     * Gets a new instance
+     *
+     * @return NavigationBuilderListener
+     */
+    private function getListener()
+    {
+        return new NavigationBuilderListener();
+    }
+}
