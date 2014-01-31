99d94e3118fa145c982450f748015da10a99b9d4
James Halsall <james.t.halsall@googlemail.com>
2014-01-31 20:49:30 +0000

    Adding RolesFormType, updating form service configuration

---
+++ src/Tickit/Bundle/UserBundle/Form/Type/RolesFormType.php
@@ -0,0 +1,107 @@
+<?php
+
+/*
+ * Tickit, an open source web based bug management tool.
+ *
+ * Copyright (C) 2014  Tickit Project <http://tickit.io>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tickit\Bundle\UserBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Security\Core\Role\RoleInterface;
+use Tickit\Component\Security\Role\Decorator\RoleDecoratorInterface;
+use Tickit\Component\Security\Role\Provider\RoleProviderInterface;
+
+/**
+ * Roles form type.
+ *
+ * A field type for managing roles on a user.
+ *
+ * @package Tickit\Bundle\UserBundle\Form\Type
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class RolesFormType extends AbstractType
+{
+    /**
+     * An array of all available roles
+     *
+     * @var RoleInterface[]
+     */
+    private $roles;
+
+    /**
+     * A role decorator
+     *
+     * @var RoleDecoratorInterface
+     */
+    private $roleDecorator;
+
+    /**
+     * Constructor
+     *
+     * @param RoleProviderInterface  $roles         A role provider
+     * @param RoleDecoratorInterface $roleDecorator A role decorator
+     */
+    public function __construct(RoleProviderInterface $roles, RoleDecoratorInterface $roleDecorator)
+    {
+        $this->roles = $roles->getRoles();
+        $this->roleDecorator = $roleDecorator;
+    }
+
+    /**
+     *
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $choices = [];
+
+        foreach ($this->roles as $role) {
+            $choices[$role->getRole()] = $this->roleDecorator->decorate($role);
+        }
+
+        $defaultOptions = [
+            'choices' => $choices,
+            'expanded' => true,
+            'multiple' => true
+        ];
+
+        $resolver->setDefaults($defaultOptions);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return string
+     */
+    public function getParent()
+    {
+        return 'choice';
+    }
+
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return 'tickit_roles';
+    }
+}
---
+++ src/Tickit/Bundle/UserBundle/Tests/Form/Type/RolesFormTypeTest.php
@@ -0,0 +1,101 @@
+<?php
+
+/*
+ * Tickit, an open source web based bug management tool.
+ *
+ * Copyright (C) 2014  Tickit Project <http://tickit.io>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tickit\Bundle\UserBundle\Tests\Form\Type;
+
+use Symfony\Component\Security\Core\Role\Role;
+use Tickit\Bundle\CoreBundle\Tests\Form\Type\AbstractFormTypeTestCase;
+use Tickit\Bundle\UserBundle\Form\Type\RolesFormType;
+
+/**
+ * RolesFormType tests
+ *
+ * @package Tickit\Bundle\UserBundle\Tests\Form\Type
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class RolesFormTypeTest extends AbstractFormTypeTestCase
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $roleProvider;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $roleDecorator;
+
+    /**
+     * Setup
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->roleProvider = $this->getMock('\Tickit\Component\Security\Role\Provider\RoleProviderInterface');
+        $this->roleDecorator = $this->getMock('\Tickit\Component\Security\Role\Decorator\RoleDecoratorInterface');
+    }
+
+    /**
+     * Tests the submit() method
+     */
+    public function testFieldBuildsWithCorrectRoles()
+    {
+        $roles = [
+            new Role('ROLE_USER'), new Role('ROLE_ADMIN'), new Role('ROLE_SUPER_ADMIN')
+        ];
+
+        $this->roleProvider->expects($this->once())
+                           ->method('getRoles')
+                           ->will($this->returnValue($roles));
+
+        $this->roleDecorator->expects($this->exactly(count($roles)))
+                            ->method('decorate')
+                            ->will($this->onConsecutiveCalls('default', 'admin', 'super admin'));
+
+        foreach ($roles as $index => $role) {
+            $this->roleDecorator->expects($this->at($index))
+                                ->method('decorate')
+                                ->with($role);
+        }
+
+        $form = $this->factory->create($this->getFormType());
+        $choices = $form->getConfig()->getOption('choices');
+
+        $this->assertCount(3, $choices);
+        $expectedChoices = [
+            'ROLE_USER' => 'default',
+            'ROLE_ADMIN' => 'admin',
+            'ROLE_SUPER_ADMIN' => 'super admin'
+        ];
+        $this->assertEquals($expectedChoices, $choices);
+    }
+
+    /**
+     * Gets a new instance of the form type
+     *
+     * @return RolesFormType
+     */
+    private function getFormType()
+    {
+        return new RolesFormType($this->roleProvider, $this->roleDecorator);
+    }
+}
