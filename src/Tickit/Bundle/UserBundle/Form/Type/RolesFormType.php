aa05d79aa5252430b7a6065ab08f8b8e2a489d10
James Halsall <james.t.halsall@googlemail.com>
2014-02-03 20:01:15 +0000

    Removing unused 'use' statements

--- src/Tickit/Bundle/UserBundle/Form/Type/RolesFormType.php
+++ src/Tickit/Bundle/UserBundle/Form/Type/RolesFormType.php
@@ -22,13 +22,9 @@
 namespace Tickit\Bundle\UserBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Symfony\Component\Security\Core\Role\RoleInterface;
 use Symfony\Component\Security\Core\SecurityContextInterface;
 use Tickit\Component\Security\Role\Decorator\RoleDecoratorInterface;
 use Tickit\Component\Security\Role\Provider\RoleProviderInterface;
