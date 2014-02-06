9d80f792b0f994fd8a7c25cd817567cdd6c87178
James Halsall <james.t.halsall@googlemail.com>
2014-02-06 21:29:47 +0000

    Moving our assets (less, fonts and images) to an AssetsBundle to ease assetic dumping of assets

---
+++ src/Tickit/Bundle/AssetsBundle/AssetsBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tickit\Bundle\AssetsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Description
+ *
+ * @package Namespace\Class
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ */
+class AssetsBundle extends Bundle
+{
+
+} 
