0086b687999db063a01e672eebda6b81919db633
Mark Wilson <mark@89allport.co.uk>
2014-03-18 20:37:16 +0000

    Add identifiable interface for entities

---
+++ src/Tickit/Component/Entity/IdentifiableInterface.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * Tickit, an open source web based bug management tool.
+ *
+ * Copyright (C) 2014  Tickit Project <http://tickit.io>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tickit\Component\Avatar\Entity;
+
+/**
+ * Identifiable interface for entities
+ *
+ * @package Tickit\Component\Avatar\Entity
+ * @author  Mark Wilson <mark@89allport.co.uk>
+ */
+interface IdentifiableInterface
+{
+    /**
+     * Get value to identify this entity
+     *
+     * @return integer
+     */
+    public function getId();
+}
